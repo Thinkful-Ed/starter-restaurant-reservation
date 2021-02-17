b7318c36e6dbe30a1d365cf5231d3c4a0d9c8a27
Dale Lotts <dlotts@knightrider.com>
2021-02-17 00:41:50 -0700

    fix: error handler (#5)

--- back-end/src/errors/errorHandler.js
+++ back-end/src/errors/errorHandler.js
@@ -1,7 +1,7 @@
 /**
  * Express API error handler.
  */
-function errorHandler(error, request, response) {
+function errorHandler(error, request, response, next) {
   const { status = 500, message = "Something went wrong!" } = error;
   response.status(status).json({ error: message });
 }
