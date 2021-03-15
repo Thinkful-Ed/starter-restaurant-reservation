9174dd9cd9d521ec6d63b99faa71b8a568c6a214
Gabriel <gabrielsz87@gmail.com>
2021-03-15 16:24:59 -0600

    Add input elements to form

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -1,8 +1,19 @@
 import React from "react"
 
 function ReservationForm(){
+
     return (
-        <h1>Reserve a table!</h1>
+        <form>
+            <input name="first_name" />
+            <input name="last_name" />
+            <input name="mobile_number" />
+            <input name="reservation_date" />
+            <input name="reservation_time" />
+            <input name="people" />
+            <button type="submit">Submit</button>
+            <button type="cancel">Cancel</button>
+
+        </form>
     )
 }
 
