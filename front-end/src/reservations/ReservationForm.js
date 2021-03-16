cc4c46eeb1b00cbdfe21a4dbeaa25ed6a2c1f348
Gabriel <gabrielsz87@gmail.com>
2021-03-15 18:03:56 -0600

    Add form formatting

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -1,22 +1,49 @@
 import React from "react"
 import { useHistory } from "react-router-dom";
 
-
 function ReservationForm(){
     const history = useHistory();
 
     return (
         <form>
-            <input name="first_name" />
-            <input name="last_name" />
-            <input name="mobile_number" />
-            <input name="reservation_date" />
-            <input name="reservation_time" />
-            <input name="people" />
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">First name:</label>
+                <div class="col-sm-10">
+                    <input name="first_name" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Last name:</label>
+                <div class="col-sm-10">
+                    <input name="last_name" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Mobile Number:</label>
+                <div class="col-sm-10">
+                    <input name="mobile_number" type="tel" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Reservation Date:</label>
+                <div class="col-sm-10">
+                    <input name="reservation_date" type="date"/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Time:</label>
+                <div class="col-sm-10">
+                    <input name="reservation_time" type="time" />
+                </div>
+            </div>
+            <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Number of people:</label>
+                <div class="col-sm-10">
+                    <input name="people" />
+                </div>
+            </div>
             <button type="submit">Submit</button>
-            <button type="button" onClick={() => history.goBack()}>
-              Cancel
-            </button>
+            <button type="button" onClick={() => history.goBack()}>Cancel</button>
         </form>
     )
 }
--- front-end/src/reservations/ReservationNew.js
+++ front-end/src/reservations/ReservationNew.js
@@ -1,11 +1,10 @@
 import React from "react"
 import ReservationForm from "./ReservationForm";
 
-
 function ReservationNew() {
   return (
     <main>
-      <h1>New Reservation</h1>
+      <h1> Create a New Reservation </h1>
       <ReservationForm />
     </main>
   );
