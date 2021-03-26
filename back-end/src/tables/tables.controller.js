d35198e8f09e76a0a2fcd277b8ed69e53dbd3f15
Gabriel <gabrielsz87@gmail.com>
2021-03-25 22:35:40 -0600

    Add validation

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,6 +1,54 @@
 const service = require("./tables.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+function hasValidFields(req, res, next) {
+  const { data = {} } = req.body;
+  const validFields = new Set([
+    "table_name",
+    "capacity",
+  ]);
+
+  const invalidFields = Object.keys(data).filter(
+    field => !validFields.has(field)
+  );
+
+  if (invalidFields.length)
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  next();
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+const has_table_name = bodyDataHas("table_name");
+const has_capacity = bodyDataHas("capacity");
+
+function isValidTableName(req, res, next){
+  const { data = {} } = req.body;
+  if (data['table_name'].length < 2){
+      return next({ status: 400, message: `table_name length is too short.` });
+  }
+  next();
+}
+
+function isValidNumber(req, res, next){
+  const { data = {} } = req.body;
+  if (data['capacity'] === 0 || !Number.isInteger(data['capacity'])){
+      return next({ status: 400, message: `capacity must be a number.` });
+  }
+  next();
+}
+
 async function list(req, res) {
   const data = await service.list(req.query.date);
 
@@ -20,6 +68,11 @@ async function create(req, res) {
 
 module.exports = {
   create: [
+      has_table_name,
+      has_capacity,
+      isValidNumber,
+      isValidTableName,
+      hasValidFields,
       asyncErrorBoundary(create)
   ],
   list: [asyncErrorBoundary(list)],
