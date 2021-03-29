e1dffc099d29a16fda6d38a39cb7afe9fce2343b
Jon Belaire <jon.belaire@icloud.com>
2021-03-29 19:35:36 -0400

    fixed next/previous in date-time

--- front-end/src/utils/date-time.js
+++ front-end/src/utils/date-time.js
@@ -57,8 +57,7 @@ export function today() {
  *  the date one day prior to currentDate, formatted as YYYY-MM-DD
  */
 export function previous(currentDate) {
-  const array = currentDate.split("-");
-  let [ year, month, day ] = array;
+  let [ year, month, day ] = currentDate.split("-");
   month -= 1;
   const date = new Date(year, month, day);
   date.setMonth(date.getMonth());
@@ -74,8 +73,7 @@ export function previous(currentDate) {
  *  the date one day after currentDate, formatted as YYYY-MM-DD
  */
 export function next(currentDate) {
-  const array = currentDate.split("-");
-  let [ year, month, day ] = array;
+  let [ year, month, day ] = currentDate.split("-");
   month -= 1;
   const date = new Date(year, month, day);
   date.setMonth(date.getMonth());
