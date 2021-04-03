178384c41c3f0a3ad7d9824461e5d42dd148f6ea
Gabriel <gabrielsz87@gmail.com>
2021-04-02 18:52:29 -0600

    Add backend validations

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -5,6 +5,7 @@ const tableName = "reservations";
 function list(date) {
   return knex(tableName)
     .where("reservation_date", date)
+    .whereNotIn("status", ["finished", "cancelled"])
     .orderBy("reservation_time");
 }
 
@@ -20,8 +21,32 @@ function read(reservation_id){
       .first();
 }
 
+function update(reservation) {
+  return knex(tableName)
+    .where({ reservation_id: reservation.reservation_id })
+    .update(reservation, "*")
+    .then((records) => records[0]);
+}
+
+function status(reservation) {
+    update(reservation);
+    return validStatus(reservation);
+}
+
+function validStatus(reservation) {
+  if (["booked", "seated", "finished", "cancelled"].includes(reservation.status)) {
+    return reservation;
+  }
+  const error = new Error(
+    `Invalid status:"${reservation.status}"`
+  );
+  error.status = 400;
+  throw error;
+}
+
 module.exports = {
   create,
   list,
   read,
+  status
 };
