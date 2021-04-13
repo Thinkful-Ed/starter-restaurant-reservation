a5bd5437259b7b51337cb35d67e71587bdc881cf
byronmosley <mosleyb91@outlook.com>
2021-04-13 08:52:28 -0700

    finished db files to pass tests, complete user story 01 backend

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,10 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("reservations", (table) => {
-    table.increments("reservation_id").primary();
-    table.timestamps(true, true);
-  });
-};
+  return knex.schema.createTable('reservations', (table) => {
+    table.increments('reservation_id').primary()
+    table.string('first_name')
+    table.string('last_name')
+    table.string('mobile_number')
+    table.date('reservation_date')
+    table.time('reservation_time')
+    table.integer('people')
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("reservations");
-};
+  return knex.schema.dropTable('reservations')
+}
