2fc1869657ce91b368fe1ae2b6f09b68eca75013
Keith Frazier <keithers98@gmail.com>
2021-06-12 16:28:29 -0700

    added form (no styling)

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -2,6 +2,12 @@ exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
     table.timestamps(true, true);
+    table.string("first_name");
+    table.string("last_name");
+    table.string("mobile_number");
+    table.string("people");
+    table.string("reservation_date")
+    table.string("reservation_time");
   });
 };
 
