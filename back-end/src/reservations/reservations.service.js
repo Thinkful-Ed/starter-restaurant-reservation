f53ce67be22333b1020517edf8ee5583e5938e38
SeanCassone <sean.cassone@gmail.com>
2021-06-15 11:25:47 -0400

    added reservations.service.js

---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,11 @@
+const knex = require("../db/connection");
+
+async function list() {}
+
+async function create() {}
+
+async function read() {}
+
+async function update() {}
+
+module.exports = {};
