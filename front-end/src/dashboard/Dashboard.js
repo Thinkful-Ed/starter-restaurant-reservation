7d9045095583f91fa6354f51f806c83e768d6c0d
SeanCassone <sean.cassone@gmail.com>
2021-06-17 00:51:19 -0400

    fixed reservations component to display cards correctly

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
-
 /**
  * Defines the dashboard page.
  * @param date
@@ -22,16 +21,62 @@ function Dashboard({ date }) {
       .catch(setReservationsError);
     return () => abortController.abort();
   }
+  const reservationCards = reservations.map(
+    ({
+      reservation_id,
+      first_name,
+      last_name,
+      mobile_number,
+      reservation_date,
+      reservation_time,
+      people,
+    }) => {
+      return (
+        <div
+          className="card border-success mb-3"
+          style={{ maxWidth: "40rem" }}
+          key={reservation_id}
+        >
+          <div className="card-header bg-transparent border-success">
+            {reservation_date}
+          </div>
+          <div className="card-body text-success">
+            <h5 className="card-title">{reservation_time}</h5>
+            <p className="card-text">
+              {first_name} {last_name}
+            </p>
+            <p className="card-text">Number of people: {people}</p>
+          </div>
+          <div className="card-footer bg-transparent border-success">
+            {mobile_number}
+          </div>
+        </div>
+      );
+    }
+  );
 
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
-      </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
-    </main>
+    reservations.length && (
+      <main>
+        <h1>Dashboard</h1>
+        <div className="d-md-flex mb-3">
+          <div className="d-flex">
+            <button type="submit" className="btn btn-primary ml-1 mt-2">
+              Today
+            </button>
+            <button type="submit" className="btn btn-danger ml-1 mt-2">
+              Tomorrow
+            </button>
+            <button type="submit" className="btn btn-danger ml-1 mt-2">
+              Yesterday
+            </button>
+          </div>
+          {/* Button Group */}
+        </div>
+        <ErrorAlert error={reservationsError} />
+        <div className="card-columns">{reservationCards}</div>
+      </main>
+    )
   );
 }
 
