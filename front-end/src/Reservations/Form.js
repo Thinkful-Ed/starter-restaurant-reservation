bc292067d95fdaa4f53ad16a55764cf849129c22
SeanCassone <sean.cassone@gmail.com>
2021-06-20 13:33:58 -0400

    reordered forms input values for date time

--- front-end/src/Reservations/Form.js
+++ front-end/src/Reservations/Form.js
@@ -83,8 +83,8 @@ function Form({ reservation, handleChange, submitHandler, cancel }) {
             id="reservation_date"
             name="reservation_date"
             type="date"
-            pattern="\d{4}-\d{2}-\d{2}"
             placeholder="YYYY-MM-DD"
+            pattern="\d{4}-\d{2}-\d{2}"
             required={true}
             defaultValue={reservation_date}
             aria-label="Reservation date"
@@ -99,8 +99,8 @@ function Form({ reservation, handleChange, submitHandler, cancel }) {
             id="reservation_time"
             name="reservation_time"
             type="time"
-            pattern="[0-9]{2}:[0-9]{2}"
             placeholder="HH:MM"
+            pattern="[0-9]{2}:[0-9]{2}"
             required={true}
             defaultValue={reservation_time}
             aria-label="Reservation time"
