7686432b7f4a26377d6088b6da485c8ed6ca6391
ryandavidmercado <ryandavidmercado@gmail.com>
2021-06-30 23:48:49 -0400

    Define list function in controller.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,12 @@
+const service = require("./reservations.service");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
+  const { date = "" } = req.query;
   res.json({
-    data: [],
+    data: date.length ? await service.listByDate(date) : await service.list(),
   });
 }
 
