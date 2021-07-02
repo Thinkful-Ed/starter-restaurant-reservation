6ce52ae1ec7d2ea23dae3d543258ac78cebd3440
Michael Malamud <michael.malamud@outlook.com>
2021-07-01 19:32:19 -0500

    all frontend and backend tests passing for US-04

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,17 +1,24 @@
 const tablesService = require("./tables.service");
-const reservationsService = require("../reservations/reservations.service")
+const reservationsService = require("../reservations/reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-
-
+function reservationIdExists(req, res, next) {
+  if (!(req.body.data) || !(req.body.data.reservation_id)) {
+    return next({
+      status: 400,
+      message: "reservation_id must exist."
+    })
+  }
+  next();
+}
 async function reservationExists(req, res, next) {
   const { reservation_id } = req.body.data;
   const reservation = await reservationsService.read(reservation_id);
   if (reservation) {
-    res.locals.reservation = reservation
+    res.locals.reservation = reservation;
     return next();
   }
-  next({ status: 404, message: `Reservation cannot be found.` });
+  next({ status: 404, message: `Reservation ${reservation_id} cannot be found.` });
 }
 
 async function tableExists(req, res, next) {
@@ -28,28 +35,69 @@ function isNotOccupied(req, res, next) {
   if (res.locals.table.status !== "Free") {
     return next({
       status: 400,
-      message: "Table is occupied."
-    })
+      message: "Table is occupied.",
+    });
   }
   next();
 }
 
+function hasSufficientCapacity(req, res, next) {
+  if (res.locals.reservation.people > res.locals.table.capacity) {
+    return next({
+      status: 400,
+      message: "Party must not be larger than capacity.",
+    });
+  }
+  next();
+}
+
+function isValidTable(req, res, next) {
+
+  const requiredFields = [
+    "table_name",
+    "capacity"
+  ]
+  if (!req.body.data) {
+     return next({
+      status: 400,
+      message: `Can not submit empty form.`,
+    });
+  }
+  for (const field of requiredFields) {
+    if (!req.body.data[field]) {
+       return next({
+        status: 400,
+        message: `Order must include a ${field}`,
+      });
+    }
+  }
+
+  if (req.body.data.table_name.length <= 1) {
+    return next({
+      status: 400,
+      message: `table_name must be longer than 1 character.`,
+    });
+  }
+  next();
+}
+
+
+
 async function update(req, res, next) {
   const { reservation_id } = req.body.data;
-  if (res.locals.reservation.people <= res.locals.table.capacity) {
   const updatedTable = {
-    ...res.locals.table, reservation_id: reservation_id, status: "Occupied"
+    ...res.locals.table,
+    reservation_id: reservation_id,
+    status: "Occupied",
   };
   const data = await tablesService.update(updatedTable);
   res.status(200).json({ data });
 }
-}
-
 
 async function create(req, res) {
   res.status(201).json({
-    data: await tablesService.create(req.body.data)
-  })
+    data: await tablesService.create(req.body.data),
+  });
 }
 
 async function list(req, res) {
@@ -59,7 +107,15 @@ async function list(req, res) {
   });
 }
 
-module.exports = { 
-  update: [asyncErrorBoundary(reservationExists), asyncErrorBoundary(tableExists), isNotOccupied, asyncErrorBoundary(update)],
-  create: asyncErrorBoundary(create),
-  list: asyncErrorBoundary(list) };
+module.exports = {
+  update: [
+    reservationIdExists,
+    asyncErrorBoundary(reservationExists),
+    asyncErrorBoundary(tableExists),
+    isNotOccupied,
+    hasSufficientCapacity,
+    asyncErrorBoundary(update),
+  ],
+  create: [isValidTable, asyncErrorBoundary(create)],
+  list: asyncErrorBoundary(list),
+};
