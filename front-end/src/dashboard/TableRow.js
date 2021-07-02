b9457602ec443c8671b08f965ca51ef9913279e7
Levi <levijackmason@gmail.com>
2021-07-01 23:35:15 -0500

    finished

--- front-end/src/dashboard/TableRow.js
+++ front-end/src/dashboard/TableRow.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 
 export default function TableRow({ table }) {
+    const history = useHistory();
+
 	if(!table) return null;
 
+	function handleFinish() {
+		if(window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+			history.push("/dashboard");
+		}
+	}
 	return (
 		<tr>
-			<th scope="row">{table.table_id}</th>
-			<td>{table.table_name}</td>
-			<td>{table.capacity}</td>
-			
-			<td data-table-id-status={table.table_id}>{table.status}</td>
-		</tr>
+		<th scope="row">{table.table_id}</th>
+		<td>{table.table_name}</td>
+		<td>{table.capacity}</td>
+		<td data-table-id-status={table.table_id}>{table.status}</td>
+
+		{table.status === "occupied" &&
+			<td data-table-id-finish={table.table_id}>
+				<button onClick={handleFinish} type="button">Finish</button>
+			</td>
+		}
+	</tr>
 	);
 }
