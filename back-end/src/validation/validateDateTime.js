ff88d95e04759e3bbe3669ec60406bdbc09b9ed8
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-03 15:30:23 -0400

    Properly wrap errors.

--- back-end/src/validation/validateDateTime.js
+++ back-end/src/validation/validateDateTime.js
@@ -1,17 +1,20 @@
 const dayjs = require("dayjs");
 const parseDateTime = require("../utils/parse-dateTime");
+const errWraper = (message) => ({ status: 400, message });
 
 const notTuesday = (req, res, next) => {
   const { reservation_date: date, reservation_time: time } = req.body.data;
   return parseDateTime(date, time).day() !== 2
     ? next()
-    : next("Reservations cannot be made for a Tuesday.");
+    : next(errWraper("Reservations cannot be made for a Tuesday."));
 };
 const isFuture = (req, res, next) => {
   const { reservation_date: date, reservation_time: time } = req.body.data;
   return parseDateTime(date, time).isAfter(dayjs(), "minute")
     ? next()
-    : next("Reservations must be made for a future date and/or time.");
+    : next(
+        errWraper("Reservations must be made for a future date and/or time.")
+      );
 };
 
 export { notTuesday, isFuture };
