5425620dbb1ddf0f1c7a727a3e71ac5d297bac43
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-03 15:13:30 -0400

    Rework validation to use dateTime parser.

--- front-end/src/utils/validation/validateDateTime.js
+++ front-end/src/utils/validation/validateDateTime.js
@@ -1,6 +1,8 @@
 const dayjs = require("dayjs");
+const parseDateTime = require("../parse-dateTime");
 
-const notTuesday = (dateTime) => dateTime.day() !== 2;
-const isFuture = (dateTime) => dateTime.isAfter(dayjs(), "minute");
+const notTuesday = (date, time) => parseDateTime(date, time).day() !== 2;
+const isFuture = (date, time) =>
+  parseDateTime(date, time).isAfter(dayjs(), "minute");
 
 export { notTuesday, isFuture };
