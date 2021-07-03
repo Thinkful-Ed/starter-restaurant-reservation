805fb9d4ab7b14f2b1313ff0129f4cd93523b8c7
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-03 18:44:21 -0400

    Fix import bug.

--- front-end/src/utils/validation/validateDateTime.js
+++ front-end/src/utils/validation/validateDateTime.js
@@ -1,5 +1,5 @@
-const dayjs = require("dayjs");
 import { parseDateTime } from "../parse-dateTime";
+const dayjs = require("dayjs");
 
 const notTuesday = (date, time) => parseDateTime(date, time).day() !== 2;
 const isFuture = (date, time) =>
