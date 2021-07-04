5bd823ae488331a677aff40c856f28b803986459
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-03 21:09:17 -0400

    Refactor API code to point to live server in prod branch.

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,9 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_API_BASE_URL
+    : "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
