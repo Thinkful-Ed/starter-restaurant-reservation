663b0658b3d386aaebd29a3f1ccdbdf8d1fe104c
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-06 15:39:22 -0400

    Add table_id to tables.

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,10 +1,11 @@
 // stub data
 const tables = [];
+let id = 0;
 
 //stub service handlers
 const list = () => Promise.resolve(tables);
 const create = (table) => {
-  tables.push({ ...table, occupied: false });
+  tables.push({ ...table, occupied: false, table_id: id++ });
   return Promise.resolve(table);
 };
 
