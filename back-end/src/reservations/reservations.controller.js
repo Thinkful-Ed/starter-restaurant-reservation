17634103e40b6bcf4500b418c4313dbd6b080999
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-06 19:46:34 -0400

    Add single-read functionality for reservations.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -11,9 +11,21 @@ const hasFields = require("../validation/hasFields")([
 const { dateTimeMiddleware } = require("../validation/validateDateTime");
 const peopleIsNum = require("../validation/peopleIsNum");
 
-/**
- * List handler for reservation resources
- */
+// Extra validation middleware
+async function reservationExists(req, res, next) {
+  const reservation = await service.read(req.params.reservationId);
+
+  if (!reservation)
+    return next({
+      status: 404,
+      message: `Reservation with reservation_id ${req.params.reservationId} not found.`,
+    });
+
+  res.locals.reservation = reservation;
+  next();
+}
+
+// Main route handlers
 async function list(req, res) {
   const { date = "" } = req.query;
   res.json({
@@ -27,7 +39,12 @@ async function create(req, res) {
   });
 }
 
+function read(req, res) {
+  res.json({ data: res.locals.reservation });
+}
+
 module.exports = {
   list,
+  read: [reservationExists, read],
   create: [hasData, hasFields, peopleIsNum, dateTimeMiddleware, create],
 };
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,11 +4,16 @@ const list = () =>
   db("reservations")
     .select("*")
     .orderBy(["reservation_date", "reservation_time"]);
+
+const read = (reservation_id) =>
+  db("reservations").first("*").where({ reservation_id });
+
 const listByDate = (reservation_date) =>
   db("reservations")
     .select("*")
     .where({ reservation_date })
     .orderBy("reservation_time");
+
 const create = (reservation) =>
   db("reservations")
     .insert(reservation, "*")
@@ -16,6 +21,7 @@ const create = (reservation) =>
 
 module.exports = {
   list,
+  read,
   listByDate,
   create,
 };
