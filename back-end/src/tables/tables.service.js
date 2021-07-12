493d2038b99f5e4a261ea9660c1937658205482c
ryandavidmercado <ryandavidmercado@gmail.com>
2021-07-12 12:32:40 -0400

    Automate update of reservation status when tables are seated or finished.

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -8,9 +8,35 @@ const create = (table) =>
     .then((res) => res[0]);
 
 const seatReservation = (table_id, reservation_id) =>
-  db("tables").where({ table_id }).update({ reservation_id }, "*");
+  db.transaction((trx) =>
+    trx("tables")
+      .where({ table_id })
+      .update({ reservation_id }, "*")
+      .then((res) => res[0])
+      .then(({ reservation_id }) =>
+        trx("reservations")
+          .where({ reservation_id })
+          .update({ status: "seated" }, "*")
+      )
+  );
 
-const finishTable = (table_id) =>
-  db("tables").where({ table_id }).update({ reservation_id: null });
+const finishTable = async (table_id) =>
+  await db.transaction(async (trx) => {
+    const reservation_id = await trx("tables")
+      .first("reservation_id")
+      .where({ table_id })
+      .then(({ reservation_id }) => reservation_id);
+
+    const finishedTable = await trx("tables")
+      .where({ table_id })
+      .update({ reservation_id: null }, "*")
+      .then((res) => res[0]);
+
+    await trx("reservations")
+      .where({ reservation_id })
+      .update({ status: "finished" });
+
+    return finishedTable;
+  });
 
 module.exports = { list, read, create, seatReservation, finishTable };
---
+++ back-end/src/validation/reservations/validStatus.js
@@ -0,0 +1,15 @@
+const validStatus = (req, res, next) => {
+  const { status } = req.body.data;
+  const validEntries = ["booked", "seated", "finished"];
+
+  return validEntries.includes(status)
+    ? next()
+    : next({
+        status: 400,
+        message: `The status must be a string that exactly matches one of the following: ${validEntries
+          .map((entry) => `'${entry}'`)
+          .join(", ")}.`,
+      });
+};
+
+module.exports = { validStatus };
