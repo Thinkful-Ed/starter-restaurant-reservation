fcc5ac762879f63bfe498446cab32420d5b94b06
Matthew Gammon <mgammon91@gmail.com>
2021-08-24 15:34:05 -0400

    added basic create functionality to controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,21 @@
+const service = require('./reservations.service');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
   res.json({
-    data: [],
+    data: await service.list(),
   });
 }
 
+async function create(req, res, next) {
+  const newReservation = await service.create(req.body.data);
+  res.status(201).json({ data: newReservation });
+}
+
 module.exports = {
-  list,
+  create: asyncErrorBoundary(create),
+  list: asyncErrorBoundary(list),
 };
