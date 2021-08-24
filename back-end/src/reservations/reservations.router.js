23053ace1761db0cd3cf77e23db8e28dc8aaf32f
Matthew Gammon <mgammon91@gmail.com>
2021-08-24 15:36:59 -0400

    added path to reservations/new with create functionality

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,9 +4,12 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./reservations.controller");
+const router = require('express').Router();
+const controller = require('./reservations.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
+const cors = require('cors');
 
-router.route("/").get(controller.list);
+router.route('/').get(controller.list);
+router.route('/new').post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
