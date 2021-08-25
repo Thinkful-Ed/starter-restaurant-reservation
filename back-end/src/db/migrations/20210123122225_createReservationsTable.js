3b9a52948e0af88c9c47c3a4e938a2ad3a6c35cd
Matthew Gammon <mgammon91@gmail.com>
2021-08-25 10:27:14 -0400

    changed column type from 'datetime' to 'date'

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('mobile_number').notNullable();
-    table.datetime('reservation_date').notNullable();
+    table.date('reservation_date').notNullable();
     table.time('reservation_time').notNullable();
     table.integer('people').notNullable();
     table.timestamps(true, true);
