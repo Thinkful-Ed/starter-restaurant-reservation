548d95bc6bd5c4e0188686bb530672bf4e17427c
Matthew Gammon <mgammon91@gmail.com>
2021-08-25 16:40:22 -0400

    removed '/new' from /reservations path

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -69,7 +69,7 @@ export async function listReservations(params, signal) {
 }
 
 export async function createReservation(reservation, signal) {
-  const url = `${API_BASE_URL}/reservations/new`;
+  const url = `${API_BASE_URL}/reservations`;
   const options = {
     method: 'POST',
     headers,
