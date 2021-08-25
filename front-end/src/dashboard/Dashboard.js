b4408ecd210a4c2d685221a800e93ea3564c2da8
Matthew Gammon <mgammon91@gmail.com>
2021-08-25 10:35:52 -0400

    prettier changed double quotes from original code into single quotes. only change

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
+import React, { useEffect, useState } from 'react';
+import { listReservations } from '../utils/api';
+import ErrorAlert from '../layout/ErrorAlert';
 
 /**
  * Defines the dashboard page.
