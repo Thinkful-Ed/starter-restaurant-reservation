c6ad018d17527e7da2359fe50de0cff122b36761
Derek Blauser <derek.blauser@gmail.com>
2021-09-30 12:51:13 -0500

    completed front end of US1

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -2,6 +2,12 @@ exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
     table.timestamps(true, true);
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.integer("people").notNullable();
+    table.date("reservation_date").notNullable();
+    table.time("reservation_time").notNullable();
   });
 };
 
