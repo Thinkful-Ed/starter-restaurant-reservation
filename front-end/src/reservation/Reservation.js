007fbc20ec0b915bcdad8d00912f295dc2d903f9
Derek Blauser <derek.blauser@gmail.com>
2021-09-30 14:36:42 -0500

    Completed userstory3 tests

--- front-end/src/reservation/Reservation.js
+++ front-end/src/reservation/Reservation.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import ErrorAlert from "../layout/ErrorAlert";
-import { today } from "../utils/date-time";
+import { createReservation } from "../utils/api";
 
-function Reservation() {
+function Reservation({ loadDashboard }) {
   const history = useHistory();
   const initialFormState = {
     first_name: null,
@@ -14,6 +14,7 @@ function Reservation() {
     people: null,
   };
 
+  const [apiError, setApiError] = useState(null);
   const [formData, setFormData] = useState({ ...initialFormState });
   const [errors, setErrors] = useState([]);
 
@@ -25,17 +26,39 @@ function Reservation() {
     });
   };
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
-    if (validateDate()) {
-      history.push(`/dashboard?date=${formData.reservation_date}`);
-    }
-  };
-
-  function validateDate() {
-    const reserveDate = new Date(formData.reservation_date);
-    const todaysDate = new Date();
     const foundErrors = [];
+    const abortController = new AbortController();
+
+    if (validateFields(foundErrors) && validateDate(foundErrors)) {
+      createReservation(formData, abortController.signal)
+        .then(loadDashboard)
+        .then(() =>
+          history.push(`/dashboard?date=${formData.reservation_date}`)
+        )
+        .catch(setApiError);
+    }
+
+    setErrors(foundErrors);
+  }
+
+  function validateFields(foundErrors) {
+    for (const field in formData) {
+      if (formData[field] === "") {
+        foundErrors.push({
+          message: `${field.split("_").join(" ")} cannot be left blank.`,
+        });
+      }
+      return foundErrors.length === 0;
+    }
+  }
+
+  function validateDate(foundErrors) {
+    const reserveDate = new Date(
+      `${formData.reservation_date}T${formData.reservation_time}:00.000`
+    );
+    const todaysDate = new Date();
 
     if (reserveDate.getDay() === 2) {
       foundErrors.push({
@@ -50,110 +73,131 @@ function Reservation() {
       });
     }
 
-    setErrors(foundErrors);
+    if (
+      reserveDate.getHours() < 10 ||
+      (reserveDate.getHours() === 10 && reserveDate.getMinutes() < 30)
+    ) {
+      foundErrors.push({
+        message:
+          "Reservation cannot be made: Restaurant is not open until 10:30AM.",
+      });
+    } else if (
+      reserveDate.getHours() > 22 ||
+      (reserveDate.getHours() === 22 && reserveDate.getMinutes() >= 30)
+    ) {
+      foundErrors.push({
+        message:
+          "Reservation cannot be made: Restaurant is closed after 10:30PM.",
+      });
+    } else if (
+      reserveDate.getHours() > 21 ||
+      (reserveDate.getHours() === 21 && reserveDate.getMinutes() > 30)
+    ) {
+      foundErrors.push({
+        message:
+          "Reservation cannot be made: Reservation must be made at least an hour before closing (10:30PM).",
+      });
 
-    if (foundErrors.length > 0) {
-      return false;
+      return foundErrors.length === 0;
     }
-    return true;
   }
-
   const reserveErrors = () => {
-    return errors.map((error, idx) => <ErrorAlert key={idx} error={error} />);
-  }
+    return errors.map((error, id) => <ErrorAlert key={id} error={error} />);
+  };
 
   return (
-      <form onSubmit={handleSubmit}>
-        {reserveErrors()}
-        <div className="form-group">
-          <label htmlFor="name">First Name:</label>
-          <input
-            className="form-control"
-            id="name"
-            name="first_name"
-            type="text"
-            placeholder="First Name"
-            value={formData.first_name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Last Name:</label>
-          <input
-            className="form-control"
-            id="name"
-            name="last_name"
-            placeholder="Last Name"
-            value={formData.last_name}
-            onChange={handleChange}
-            required
-          ></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="tel">Mobile Number:</label>
-          <input
-            className="form-control"
-            id="tel"
-            name="mobile_number"
-            type="tel"
-            placeholder="xxx-xxx-xxxx"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            value={formData.mobile_number}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="date">Date of Reservation:</label>
-          <input
-            className="form-control"
-            id="date"
-            name="reservation_date"
-            type="date"
-            value={formData.reservation_date}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="time">Time of Reservation:</label>
-          <input
-            className="form-control"
-            id="time"
-            name="reservation_time"
-            type="time"
-            value={formData.reservation_time}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="tel">Party Size:</label>
-          <input
-            className="form-control"
-            id="size"
-            name="people"
-            type="number"
-            value={formData.people}
-            onChange={handleChange}
-            min="1"
-            step="1"
-            required
-          />
-        </div>
-        <br />
-        <button
-          onClick={() => history.push("/")}
-          type="button"
-          className="btn btn-secondary mr-2"
-        >
-          Cancel
-        </button>
-        <button className="btn btn-primary" type="submit" value="Submit">
-          Submit
-        </button>
-      </form>
+    <form onSubmit={handleSubmit}>
+      {reserveErrors()}
+      <ErrorAlert error={apiError} />
+      <div className="form-group">
+        <label htmlFor="name">First Name:</label>
+        <input
+          className="form-control"
+          id="name"
+          name="first_name"
+          type="text"
+          placeholder="First Name"
+          value={formData.first_name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Last Name:</label>
+        <input
+          className="form-control"
+          id="name"
+          name="last_name"
+          placeholder="Last Name"
+          value={formData.last_name}
+          onChange={handleChange}
+          required
+        ></input>
+      </div>
+      <div className="form-group">
+        <label htmlFor="tel">Mobile Number:</label>
+        <input
+          className="form-control"
+          id="tel"
+          name="mobile_number"
+          type="tel"
+          placeholder="xxx-xxx-xxxx"
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          value={formData.mobile_number}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="date">Date of Reservation:</label>
+        <input
+          className="form-control"
+          id="date"
+          name="reservation_date"
+          type="date"
+          value={formData.reservation_date}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="time">Time of Reservation:</label>
+        <input
+          className="form-control"
+          id="time"
+          name="reservation_time"
+          type="time"
+          value={formData.reservation_time}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="tel">Party Size:</label>
+        <input
+          className="form-control"
+          id="size"
+          name="people"
+          type="number"
+          value={formData.people}
+          onChange={handleChange}
+          min="1"
+          step="1"
+          required
+        />
+      </div>
+      <br />
+      <button
+        onClick={() => history.push("/")}
+        type="button"
+        className="btn btn-secondary mr-2"
+      >
+        Cancel
+      </button>
+      <button className="btn btn-primary" type="submit" value="Submit">
+        Submit
+      </button>
+    </form>
   );
 }
 
--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -67,3 +67,10 @@ export async function listReservations(params, signal) {
     .then(formatReservationDate)
     .then(formatReservationTime);
 }
+
+// create a new reservation
+export async function createReservation(reservation, signal) {
+  const url = `${API_BASE_URL}/reservations`;
+  const body = JSON.stringify({ data: reservation });
+  return await fetchJson(url, { headers, signal, method: "POST", body }, []);
+}
