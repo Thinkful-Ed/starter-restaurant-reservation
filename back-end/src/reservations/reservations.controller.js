068d69a6527f221f0389b594d5b8fab6e9c35df2
Donald Farrar <donfar09@gmail.com>
2021-09-30 21:28:30 -0700

    updated controller and router for user story 1 also added a service file

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,121 @@
 /**
  * List handler for reservation resources
  */
+
+//VALIDATIONS/MIDDLEWARE
+function validateBody(req, res, next) {
+  if (!req.body.data) {
+    return next({ status: 400, message: "Body must include a data object" });
+  }
+
+  const requiredFields = [
+    "first_name",
+    "last_name",
+    "mobile_number",
+    "reservation_date",
+    "reservation_time",
+    "people",
+  ];
+
+  for (const field of requiredFields) {
+    if (!req.body.data.hasOwnProperty(field) || req.body.data[field] === "") {
+      return next({ status: 400, message: `Field required: '${field}'` });
+    }
+  }
+
+  if (
+    Number.isNaN(
+      Date.parse(
+        `${req.body.data.reservation_date} ${req.body.data.reservation_time}`
+      )
+    )
+  ) {
+    return next({
+      status: 400,
+      message:
+        "'reservation_date' or 'reservation_time' field is in an incorrect format",
+    });
+  }
+
+  if (typeof req.body.data.people !== "number") {
+    return next({ status: 400, message: "'people' field must be a number" });
+  }
+
+  if (req.body.data.people < 1) {
+    return next({ status: 400, message: "'people' field must be at least 1" });
+  }
+
+  next();
+}
+
+function validateDate(req, res, next) {
+  const reserveDate = new Date(
+    `${req.body.data.reservation_date}T${req.body.data.reservation_time}:00.000`
+  );
+  const todaysDate = new Date();
+
+  if (reserveDate.getDay() === 2) {
+    return next({
+      status: 400,
+      message: "'reservation_date' field: restauraunt is closed on tuesday",
+    });
+  }
+
+  if (reserveDate < todaysDate) {
+    return next({
+      status: 400,
+      message:
+        "'reservation_date' and 'reservation_time' field must be in the future",
+    });
+  }
+
+  if (
+    reserveDate.getHours() < 10 ||
+    (reserveDate.getHours() === 10 && reserveDate.getMinutes() < 30)
+  ) {
+    return next({
+      status: 400,
+      message: "'reservation_time' field: restaurant is not open until 10:30AM",
+    });
+  }
+
+  if (
+    reserveDate.getHours() > 22 ||
+    (reserveDate.getHours() === 22 && reserveDate.getMinutes() >= 30)
+  ) {
+    return next({
+      status: 400,
+      message: "'reservation_time' field: restaurant is closed after 10:30PM",
+    });
+  }
+
+  if (
+    reserveDate.getHours() > 21 ||
+    (reserveDate.getHours() === 21 && reserveDate.getMinutes() > 30)
+  ) {
+    return next({
+      status: 400,
+      message:
+        "'reservation_time' field: reservation must be made at least an hour before closing (10:30PM)",
+    });
+  }
+
+  next();
+}
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const date = req.query.date;
+  const response = await service.list(date);
+  res.json({ data: response });
+}
+
+async function create(req, res) {
+  req.body.data.status = "booked";
+  const response = await service.create(req.body.data);
+  res.status(201).json({ data: response[0] });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
+  create: [validateBody, validateDate, asyncErrorBoundary(create)],
 };
---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,21 @@
+const knex = require("../db/connection");
+
+const tableName = "reservations";
+
+function list(date) {
+  // if a date argument was passed in, we apply that search restriction
+  if (date) {
+    return knex(tableName).select("*").where({ reservation_date: date });
+  }
+  // otherwise, just return all the reservations
+  return knex(tableName).select("*");
+}
+
+function create(reservation) {
+  return knex(tableName).insert(reservation).returning("*");
+}
+
+module.exports = {
+  list,
+  create,
+};
