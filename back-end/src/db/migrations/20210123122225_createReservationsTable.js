459f10c63f45982d72c6fe890d89bd325b846501
Donald Farrar <donfar09@gmail.com>
2021-09-30 21:14:21 -0700

    added and seed migrations

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,10 +1,19 @@
+// creating a table is pretty easy; follow this general format.
+// this function is called whenever we run a migration
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
-    table.increments("reservation_id").primary();
+    table.increments("reservation_id").primary().notNullable();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.date("reservation_date").notNullable();
+    table.time("reservation_time").notNullable();
+    table.integer("people").notNullable();
     table.timestamps(true, true);
   });
 };
 
+// this function is ran whenever migrations are rolled back
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
