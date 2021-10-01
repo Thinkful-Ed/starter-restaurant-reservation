f118f8d24ff6b00c5d496f3c3ab483c993738b0c
Donald Farrar <donfar09@gmail.com>
2021-09-30 20:44:29 -0700

    added routes

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import NewReservation from "../reservations/NewReservation";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import useQuery from "../utils/useQuery";
 
 /**
  * Defines all the routes for the application.
@@ -13,6 +14,10 @@ import { today } from "../utils/date-time";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
+  const date = query.get("date");
+  //console.log("date", date);
+
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -22,7 +27,10 @@ function Routes() {
         <Redirect to={"/dashboard"} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={date ? date : today()} />
+      </Route>
+      <Route path="/reservations/new">
+        <NewReservation />
       </Route>
       <Route>
         <NotFound />
