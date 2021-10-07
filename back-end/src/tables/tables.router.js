08bc5374f07c86a5f49ba3ae4d07d032c6fc734a
Donald Farrar <donfar09@gmail.com>
2021-10-07 09:15:15 -0700

    created router file for tables folder in the backend

---
+++ back-end/src/tables/tables.router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+module.exports = router;
