4ec80d7b4225099a95a4271847242d344048eb74
James Sobeck <sobeckjames@gmail.com>
2022-03-01 09:54:23 -0600

    updated frontend api URL

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -4,7 +4,7 @@
  */
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
-
+// api base URL
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
