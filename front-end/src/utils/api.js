1141d1b674a39581b7fb90d56aa38c3d4ab49182
Nathaniel Wills <nathanielwills708@gmail.com>
2022-04-12 08:38:45 -0700

    added comment about prod api url

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,6 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
+//production environment points to deployed backend url
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
