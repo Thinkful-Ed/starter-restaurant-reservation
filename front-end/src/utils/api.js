8a6c5bcd8d020cd9e0d92c69f6fadb1a313cdb72
James Gitchell <jamesgitchell.dev@gmail.com>
2022-04-13 15:04:16 -0400

    forced reboot for localhost reset

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,6 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
+//get API url from environment
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
