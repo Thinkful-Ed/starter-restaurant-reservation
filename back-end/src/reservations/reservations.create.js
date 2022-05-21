4a92bab4e86cbb6f3d8693320074f9bf818da5f8
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-20 22:10:52 -0400

    added env vars to gitIgnore

--- back-end/src/reservations/reservations.create.js
+++ back-end/src/reservations/reservations.create.js
@@ -25,3 +25,30 @@ function read(reservationId) {
     .where({ reservation_id: reservationId })
     .then((returnedRecords) => returnedRecords[0]);
 }
+
+//updates the status of the reservation
+function update(updateReservation) {
+    return knex("reservations")
+    .select("*")
+    .where({ reservation_id: updatedReservation.reservation_id })
+    .update(updatedReservation, "*")
+    .then((updatedReservations) => updatedReservations[0]);
+
+}
+
+function find(mobile_number) {
+    return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
+module.exports = {
+    list,
+    create, 
+    read,
+    update,
+    find,
+}
