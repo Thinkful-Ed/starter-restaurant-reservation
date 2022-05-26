543445e2ce99cb0130358fb037809af15ae11f6c
dan <da.ga.mo.94@gmail.com>
2022-05-26 01:00:40 -0600

    adjust form layout so that pressing tab will go navigate fields in the desired order

--- front-end/src/Reservations/ReservationForm.js
+++ front-end/src/Reservations/ReservationForm.js
@@ -28,90 +28,103 @@ function ReservationForm() {
         event.preventDefault();
         console.log(formData);
         createReservation(formData).then(() => {
-            setFormData({ ...INITIAL_FORM_STATE });
+            //setFormData({ ...INITIAL_FORM_STATE });
             history.push(`/dashboard?date=${formData.reservation_date}`);
         })
     }
 
     return (
         <form name="reservation" onSubmit={(handleSubmit)}>
-            <div className="form-group row">
-                <div className="col">
-                    <label htmlFor="first_name">First Name</label>
-                    <input
-                        id="first_name"
-                        name="first_name"
-                        type="text"
-                        required
-                        placeholder="First Name"
-                        onChange={handleChange}
-                        value={formData.first_name}
-                        className="form-control"
-                    />
+            <h1>Create Reservation</h1>
+            <div className="form-group">
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                            id="first_name"
+                            name="first_name"
+                            type="text"
+                            required
+                            placeholder="First Name"
+                            onChange={handleChange}
+                            value={formData.first_name}
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="last_name">Last Name</label>
+                        <input
+                            id="last_name"
+                            name="last_name"
+                            type="text"
+                            required
+                            placeholder="Last Name"
+                            onChange={handleChange}
+                            value={formData.last_name}
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="mobile_number">Mobile Number</label>
+                        <input
+                            id="mobile_number"
+                            name="mobile_number"
+                            type="text"
+                            required
+                            placeholder="###-###-####"
+                            onChange={handleChange}
+                            value={formData.mobile_number}
+                            className="form-control"
+                        />
+                    </div>
 
-                    <label htmlFor="reservation_date">Reservation Date</label>
-                    <input
-                        id="reservation_date"
-                        name="reservation_date"
-                        type="date"
-                        required
-                        placeholder="YYYY-MM-DD"
-                        pattern="\d{4}-\d{2}-\d{2}"
-                        onChange={handleChange}
-                        value={formData.reservation_date}
-                        className="form-control"
-                    />
+
                 </div>
-                <div className="col">
-                    <label htmlFor="last_name">Last Name</label>
-                    <input
-                        id="last_name"
-                        name="last_name"
-                        type="text"
-                        required
-                        placeholder="Last Name"
-                        onChange={handleChange}
-                        value={formData.last_name}
-                        className="form-control"
-                    />
-                    <label htmlFor="reservation_time">Reservation Time</label>
-                    <input
-                        id="reservation_time"
-                        name="reservation_time"
-                        type="time"
-                        required
-                        placeholder="HH:MM"
-                        pattern="[0-9]{2}:[0-9]{2}"
-                        onChange={handleChange}
-                        value={formData.reservation_time}
-                        className="form-control"
-                    />
-                </div>
-                <div className="col">
-                    <label htmlFor="mobile_number">Mobile Number</label>
-                    <input
-                        id="mobile_number"
-                        name="mobile_number"
-                        type="text"
-                        required
-                        placeholder="###-###-####"
-                        onChange={handleChange}
-                        value={formData.mobile_number}
-                        className="form-control"
-                    />
-                    <label htmlFor="people">People</label>
-                    <input
-                        id="people"
-                        name="people"
-                        type="number"
-                        min="1"
-                        step="1"
-                        placeholder={formData.people}
-                        onChange={handleChange}
-                        value={formData.people}
-                        className="form-control"
-                        required
-                    />
+                <div className="row">
+                    <div className="col">
+                        <label htmlFor="reservation_date">Reservation Date</label>
+                        <input
+                            id="reservation_date"
+                            name="reservation_date"
+                            type="date"
+                            required
+                            placeholder="YYYY-MM-DD"
+                            pattern="\d{4}-\d{2}-\d{2}"
+                            onChange={handleChange}
+                            value={formData.reservation_date}
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col">
+                        <label htmlFor="reservation_time">Reservation Time</label>
+                        <input
+                            id="reservation_time"
+                            name="reservation_time"
+                            type="time"
+                            required
+                            placeholder="HH:MM"
+                            pattern="[0-9]{2}:[0-9]{2}"
+                            onChange={handleChange}
+                            value={formData.reservation_time}
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col">
+
+                        <label htmlFor="people">People</label>
+                        <input
+                            id="people"
+                            name="people"
+                            type="number"
+                            min="1"
+                            step="1"
+                            placeholder={formData.people}
+                            onChange={handleChange}
+                            value={formData.people}
+                            className="form-control"
+                            required
+                        />
+                    </div>
                 </div>
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
