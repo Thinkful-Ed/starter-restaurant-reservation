455be75e2c378eec856a22dfd0477f5fb564df6d
David Jiang <davidjems@gmail.com>
2022-07-13 10:28:15 -0400

    day 1 start

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,6 @@
+const reservations = require("./00-reservations.json")
+//todo - seed the database given the data
+
 exports.seed = function (knex) {
   return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
 };
