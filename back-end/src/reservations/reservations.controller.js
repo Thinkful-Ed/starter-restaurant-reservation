c07805adbcc959b98a67a799b62cda94a2275240
Nahia Riviera <n.riviera.dev@gmail.com>
2022-07-14 11:16:02 -0500

    backend validation for us-01 complete

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -14,18 +14,61 @@ function checkValidFields(req, res, next) {
 		"reservation_time",
 		"people",
 	];
-  const {data} = req.body
+	const { data } = req.body;
 
 	if (data) {
 		for (let field of validFields) {
 			if (!data[field]) {
-				next({status:400,message:`${field} does not exist`})
+				return next({ status: 400, message: `${field} does not exist` });
+			}
+			if (field === "reservation_date") {
+				if (!validateDate(data[field])) {
+					return next({ status: 400, message: `${field} is not valid.` });
+				}
+			}
+			if (field === "reservation_time"){
+				if(!validateTime(data[field])){
+					return next({ status: 400, message: `${field} is not valid.` });
+				}
+			}
+			if (field === "people"){
+				if(!validatePeople(data[field])){
+					return next({ status: 400, message: `${field} is not valid.` });
+				}
 			}
 		}
 	} else {
-		next({status:400,message:"Data does not exists"});
+		return next({ status: 400, message: "Data does not exists" });
 	}
 	return next();
+}
+
+function validateDate(date) {
+	date = new Date(date);
+	if (date.toString() === "Invalid Date") {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function validateTime(time) {
+	time = time.split(":").map(value=>{return parseInt(value)})
+	if (!time[0]){return false}
+	if (time[0] < 0 || time[0] > 23){
+		return false
+	}
+	if ((time[1] < 0 || time[1] > 59)||(time[2] < 0 || time[2] > 59)){
+		return false
+	}
+	return true
+}
+
+function validatePeople(peopleCount) {
+	if (typeof peopleCount === "string" || peopleCount <= 0){
+		return false
+	}
+	return true
 }
 
 //-----------------CRUD FUNCTIONS-----------------
@@ -46,5 +89,5 @@ async function create(req, res, next) {
 
 module.exports = {
 	list,
-	create:[checkValidFields,create],
+	create: [checkValidFields, create],
 };
