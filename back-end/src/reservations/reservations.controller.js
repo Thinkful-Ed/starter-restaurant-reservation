b2434bd00ce790b61a97ff0fcc6c55eaa5561d3f
Solreth <Solsantari@gmail.com>
2022-07-17 13:00:06 -0400

    adjusted some error handling tools

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,14 @@
-/**
- * List handler for reservation resources
- */
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const data = await service.list();
+  res.json({ data });
 }
 
+async function create(req, res) {}
+
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
+  create: asyncErrorBoundary(create),
 };
