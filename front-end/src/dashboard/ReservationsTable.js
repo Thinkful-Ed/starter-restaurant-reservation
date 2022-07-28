efcf9597b74317cd8315df0f1e448ac6e19646a5
Tyler Borton <tylercborton@gmail.com>
2022-07-27 21:21:28 -0400

    further FE progress

--- front-end/src/dashboard/ReservationsTable.js
+++ front-end/src/dashboard/ReservationsTable.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 export default function ReservationsTable({reservations}) {
-    const reservationsTableRows = reservations.map((reservation) => {
+
+    const reservationsTableRow = reservations.map((reservation) => {
         return (
-            <tr key={reservation.reservation_id}>
-            <td>{reservation.reservation_id}</td>
-            <td>{reservation.last_name}, {reservation.first_name}</td>
-            <td>{reservation.mobile_number}</td>
-            <td>{reservation.reservation_date}</td>
-            <td>{reservation.reservation_time}</td>
-            <td>{reservation.people}</td>
-            </tr>
-        );
+          <tr key={reservation.reservation_id}>
+          <td>{reservation.reservation_id}</td>
+          <td>{reservation.last_name}, {reservation.first_name}</td>
+          <td>{reservation.mobile_number}</td>
+          <td>{reservation.reservation_date}</td>
+          <td>{reservation.reservation_time}</td>
+          <td>{reservation.people}</td>
+          <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
+          <td>
+          {reservation.status === "booked" ? (
+        <a
+          className="btn btn-secondary"
+          role="button"
+          href={`/reservations/${reservation.reservation_id}/seat`}
+        >
+          Seat
+        </a>
+      ) : null}
+          </td>
+          </tr>
+      );
       });
 
     return (
@@ -28,7 +41,7 @@ export default function ReservationsTable({reservations}) {
                         <th>STATUS</th>
                     </tr>
                 </thead>
-                <tbody>{reservationsTableRows}</tbody>
+                <tbody>{reservationsTableRow}</tbody>
             </table>
         </div>
     );
