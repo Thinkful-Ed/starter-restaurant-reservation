e2fd3e35f3c92d9805239958824eb051954b62c4
Tyler Borton <tylercborton@gmail.com>
2022-07-27 21:26:29 -0400

    US-04 finished

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,87 +1,131 @@
 /**
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
-*/
-
-import formatReservationDate from "./format-reservation-date";
-import formatReservationTime from "./format-reservation-date";
-
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
-
-/**
- * Defines the default headers for these functions to work with `json-server`
-*/
-
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
-
-/**
- * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
- *
- * This function is NOT exported because it is not needed outside of this file.
- *
- * @param url
- *  the url for the requst.
- * @param options
- *  any options for fetch
- * @param onCancel
- *  value to return if fetch call is aborted. Default value is undefined.
- * @returns {Promise<Error|any>}
- *  a promise that resolves to the `json` data or an error.
- *  If the response is not in the 200 - 399 range the promise is rejected.
-*/
-
-async function fetchJson(url, options, onCancel) {
-  try {
-    const response = await fetch(url, options);
-
-    if (response.status === 204) {
-      return null;
-    }
-
-    const payload = await response.json();
-
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      throw error;
-    }
-    return Promise.resolve(onCancel);
-  }
-}
-
-/**
- * Retrieves all existing reservation.
- * @returns {Promise<[reservation]>}
- *  a promise that resolves to a possibly empty array of reservation saved in the database.
-*/
-
-export async function listReservations(params, signal) {
-  const url = new URL(`${API_BASE_URL}/reservations`);
-  Object.entries(params).forEach(([key, value]) =>
-    url.searchParams.append(key, value.toString())
-  );
-  return await fetchJson(url, { headers, signal }, [])
-    .then(formatReservationDate)
-    .then(formatReservationTime);
-}
-
-/** 
- * Creates a new reservation 
-*/
-
-export async function createReservations(data, signal) {
-  const url = new URL(`${API_BASE_URL}/reservations`);
-  const options = {
-    headers, 
-    signal,
-    method: "POST",
-    body: JSON.stringify({data}),
-  }
-  return await fetchJson(url, options, data);
-}
+ */
+ import formatReservationDate from "./format-reservation-date";
+ import formatReservationTime from "./format-reservation-date";
+ 
+ const API_BASE_URL =
+   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+ 
+ /**
+  * Defines the default headers for these functions to work with `json-server`
+  */
+ const headers = new Headers();
+ headers.append("Content-Type", "application/json");
+ 
+ /**
+  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
+  *
+  * This function is NOT exported because it is not needed outside of this file.
+  *
+  * @param url
+  *  the url for the requst.
+  * @param options
+  *  any options for fetch
+  * @param onCancel
+  *  value to return if fetch call is aborted. Default value is undefined.
+  * @returns {Promise<Error|any>}
+  *  a promise that resolves to the `json` data or an error.
+  *  If the response is not in the 200 - 399 range the promise is rejected.
+  */
+ async function fetchJson(url, options, onCancel) {
+   try {
+     const response = await fetch(url, options);
+ 
+     if (response.status === 204) {
+       return null;
+     }
+ 
+     const payload = await response.json();
+ 
+     if (payload.error) {
+       return Promise.reject({ message: payload.error });
+     }
+     return payload.data;
+   } catch (error) {
+     if (error.name !== "AbortError") {
+       console.error(error.stack);
+       throw error;
+     }
+     return Promise.resolve(onCancel);
+   }
+ }
+ 
+ /**
+  * Retrieves all existing reservation.
+  * @returns {Promise<[reservation]>}
+  *  a promise that resolves to a possibly empty array of reservation saved in the database.
+  */
+ 
+ export async function listReservations(params, signal) {
+   const url = new URL(`${API_BASE_URL}/reservations`);
+   Object.entries(params).forEach(([key, value]) =>
+     url.searchParams.append(key, value.toString())
+   );
+   return await fetchJson(url, { headers, signal }, [])
+     .then(formatReservationDate)
+     .then(formatReservationTime);
+ }
+ 
+ export async function readReservation(reservation_id, signal) {
+   const url = new URL(`${API_BASE_URL}/reservations/${reservation_id}`);
+   return await fetchJson(url, { headers, signal }, [])
+   .then(formatReservationDate)
+   .then(formatReservationTime);
+ }
+ 
+ 
+ export async function createReservations(data, signal) {
+   const url = new URL(`${API_BASE_URL}/reservations`);
+   const options = {
+     headers, 
+     signal,
+     method: "POST",
+     body: JSON.stringify({data}),
+   }
+   return await fetchJson(url, options, data);
+ }
+ 
+ export async function createTable(data, signal) {
+   const url = new URL(`${API_BASE_URL}/tables`);
+   const options = {
+     headers, 
+     signal, 
+     method: "POST",
+     body: JSON.stringify({data}),
+   }
+   return await fetchJson(url, options, data);
+ }
+ 
+ export async function listTables(signal) {
+   const url = new URL(`${API_BASE_URL}/tables`);
+   return await fetchJson(url, { headers, signal }, []);
+ }
+ 
+ // export async function seatReservation(reservation_id, signal) {
+ //   const url = `${API_BASE_URL}/reservations/${reservation_id}/seat`;
+ //   const options = {
+ //     headers,
+ //     signal,
+ //     method: "PUT",
+ //     body: JSON.stringify({ reservation_id }),
+ //   };
+ //   return await fetchJson(url, options, []);
+ // }
+ 
+ export async function updateTableOnceSeated(reservation_id, table_id, signal) {
+   const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+   // console.log(reservation_id, table_id)
+   const options = {
+     headers,
+     signal,
+     method: "PUT",
+     body: JSON.stringify({ data: {reservation_id} }),
+   };
+   return await fetchJson(url, options, []);
+ }
+ 
+ 
+ 
+ 
