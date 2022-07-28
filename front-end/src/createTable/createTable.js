7122e56ae4c3a303f601bdae923da896b72e95e0
Tyler Borton <tylercborton@gmail.com>
2022-07-28 13:05:50 -0400

    all tests passing for US-04

--- front-end/src/createTable/createTable.js
+++ front-end/src/createTable/createTable.js
@@ -3,89 +3,78 @@ import { useHistory } from "react-router-dom";
 import { createTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 
+
 export default function CreateTable() {
-	const initTable = {
-		table_name: "",
-		capacity: 0,
-	};
+    const history = useHistory();
+    const initialFormState = {
+        table_name: "",
+        capacity: 0,
+    }
 
-	const history = useHistory();
+    const [formData, setFormData] = useState({...initialFormState});
+    const [reservationsError, setReservationsError] = useState(null);
 
-	const [table, setTable] = useState(initTable);
-	const [tableError, setTableError] = useState(null);
+    const changeHandler = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-	const cancelClickHandler = () => {
-		history.goBack();
-	};
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            setReservationsError(null);
+            await createTable({...formData, capacity: Number(formData.capacity)});
+            history.push("/dashboard")
+        } catch (error) {
+            setReservationsError(error);
+            console.log(error);
+        };
+        }
 
-	const submitHandler = async (event) => {
-		event.preventDefault();
-		try {
-			const abortController = new AbortController();
-			await createTable({ data: table }, abortController.signal);
-			history.push("/dashboard");
-		} catch (error) {
-			setTableError(error);
-		}
-	};
+    function cancelHandler() {
+        history.goBack();
+    }
 
-	const formChangeHandler = (event) => {
-		const tableKey = event.target.name;
-		let tableValue = event.target.value;
-		if (tableKey === "capacity" && tableValue){
-			tableValue = parseInt(tableValue)
-		}
-
-		setTable({ ...table, [tableKey]: tableValue });
-	};
-
-	return (
-		<div>
-			<h1>Add Table</h1>
-			<ErrorAlert error={tableError} />
-			<div>
-				<form onSubmit={submitHandler}>
-					<div className="mb-3">
-						<label htmlFor="table_name" className="form-label">
-							Table Name
-						</label>
-						<input
-							required
-							name="table_name"
-							type="text"
-							className="form-control"
-							minLength="2"
-							value={table.table_name}
-							onChange={formChangeHandler}
-						/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="capacity" className="form-label">
-							Capacity
-						</label>
-						<input
-							required
-							name="capacity"
-							type="number"
-							className="form-control"
-							value={table.capacity}
-							onChange={formChangeHandler}
-						></input>
-					</div>
-					<div className="mb-3">
-						<button
-							type="button"
-							className="btn btn-danger"
-							onClick={cancelClickHandler}
-						>
-							Cancel
-						</button>
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+    return (
+    <div>
+      <h1>Create Table</h1>
+      <ErrorAlert error={reservationsError} />
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="form-group col">
+            <label>
+              Table Name
+              <input
+                name="table_name"
+                type="text"
+                className="form-control"
+                required=""
+                placeholder="Table Name"
+                value={formData.table_name}
+                onChange={changeHandler}
+              ></input>
+            </label>
+          </div>
+          <div className="form-group col">
+            <label>
+              Capacity
+              <input
+                name="capacity"
+                type="number"
+                className="form-control"
+                required=""
+                placeholder=""
+                value={formData.capacity}
+                onChange={changeHandler}
+              ></input>
+            </label>
+          </div>
+        </div>
+        <button className="btn btn-secondary mr-2 cancel" type="button" onClick={cancelHandler}>Cancel</button>
+        <button className="btn btn-primary" type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
