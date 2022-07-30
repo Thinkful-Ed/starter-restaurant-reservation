d6d5fb3aef2dd8c6592ca4f610863f78b3734a43
Tyler Borton <tylercborton@gmail.com>
2022-07-30 18:39:05 -0400

    added edit and cancel button

--- front-end/src/dashboard/ReservationsTable.js
+++ front-end/src/dashboard/ReservationsTable.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
+import { updateReservationStatus } from "../utils/api";
 
-export default function ReservationsTable({reservations}) {
+export default function ReservationsTable({reservations, setError}) {
 
+    const ac = new AbortController();
+    const history = useHistory();
+
+    const handleCancel = async (event) => {
+        event.preventDefault();
+        const { reservationIdCancel } = event.target.dataset;
+    
+        if(window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
+          try {
+    
+            await updateReservationStatus({status: "cancelled"}, reservationIdCancel, ac.signal);
+            history.push("/dashboard");
+          } catch (error) {
+            setError(error);
+          }
+        }
+      };
+    
     const reservationsTableRow = reservations.map((reservation) => {
         return (
           <tr key={reservation.reservation_id}>
@@ -14,13 +33,17 @@ export default function ReservationsTable({reservations}) {
           <td>{reservation.people}</td>
           <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
           <td>{reservation.status !== "booked" ? null : (
+            <div>
             <a
             className="btn btn-secondary"
             role="button"
             href={`/reservations/${reservation.reservation_id}/seat`}
-          >
+            >
             Seat
-          </a>
+            </a>
+            <a href={`/reservations/${reservation.reservation_id}/edit`}><button className="btn btn-secondary">Edit</button></a>
+            <button data-reservation-id-cancel={reservation.reservation_id} className="btn btn-secondary" onClick={handleCancel}>Cancel</button>
+            </div>
           )}
           </td>
           </tr>
