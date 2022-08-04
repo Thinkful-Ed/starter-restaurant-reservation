1f2a22d22591d25c5d7580ad9c329d9fc6e264f9
Mercy Akindele <mercyakinlawon@gmail.com>
2022-08-04 16:51:57 -0500

    testing to see if the updated path works in heroku

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "../Dashboard/Dashboard";
+import Dashboard from "./../Dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import useQuery from "../utils/useQuery";
