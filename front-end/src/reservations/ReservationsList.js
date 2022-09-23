7901e7bf7e1e666ca01ac417aaded32dabb711c8
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-23 14:37:00 -0400

    Modified ReservationsList.js component to filter out reservations with a 'finsihed' status

--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReservationCard from "./ReservationCard";
 
 export default function ReservationsList({ reservations }) {
-  const reservationsList = reservations.map((reservation) => {
-    return (
-      <ReservationCard
-        key={reservation.reservation_id}
-        reservation={reservation}
-      />
-    );
-  });
+  const reservationsList = reservations
+    .filter((reservation) => {
+      return reservation.status !== "finished";
+    })
+    .map((reservation) => {
+      return (
+        <ReservationCard
+          key={reservation.reservation_id}
+          reservation={reservation}
+        />
+      );
+    });
 
   return (
     <div>
