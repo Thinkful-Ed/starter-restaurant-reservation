ff2f631f89ec7343456fb7f9ec7283d38c8cdbec
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-24 15:39:26 -0400

    Modified SearchForm and added formatPhoneNumber to utils

--- front-end/src/search/SearchForm.js
+++ front-end/src/search/SearchForm.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import formatPhoneNumber from "../utils/formatPhoneNumber";
 
 export default function SearchForm() {
+  const [mobileNumber, setMobileNumber] = useState("");
+
+const handleChange = (event) => {
+    const formattedPhoneNumber = formatPhoneNumber(event.target.value);
+    setMobileNumber(formattedPhoneNumber);
+  };
+
   return (
     <div>
       <form>
-        <div><p>Placeholder for Search Form</p></div>
+        <div>
+          <label htmlFor="mobile_number">Mobile Number:</label>
+          <input
+            name="mobile_number"
+            type="text"
+            id="mobile_number"
+            placeholder="Enter a customer's phone number"
+            required={true}
+            value={mobileNumber}
+            onChange={handleChange}
+          />
+        </div>
       </form>
     </div>
   );
