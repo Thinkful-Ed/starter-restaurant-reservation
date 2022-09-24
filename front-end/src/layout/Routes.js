375cd284836b40a94abcf4447877ae6431871f9c
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-24 15:19:12 -0400

    Created a new route for /search and placeholder components

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -7,6 +7,7 @@ import { today } from "../utils/date-time";
 import CreateReservation from "../reservations/CreateReservation";
 import SeatReservation from "../reservations/SeatReservation";
 import CreateTable from "../tables/CreateTable";
+import SearchReservations from "../search/SearchReservations";
 
 /**
  * Defines all the routes for the application.
@@ -35,6 +36,9 @@ function Routes() {
       </Route>
       <Route exact={true} path="/tables/new">
         <CreateTable />
+      </Route>
+      <Route exact={true} path="/search">
+        <SearchReservations />
       </Route>
       <Route>
         <NotFound />
---
+++ front-end/src/search/SearchForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchForm() {
+  return (
+    <div>
+      <form>
+        <div><p>Placeholder for Search Form</p></div>
+      </form>
+    </div>
+  );
+}
