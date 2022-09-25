1d0933504ac06c3d50c25c1e5ae7a905955b1ca1
brianwmorgan <brianwmorgan317@gmail.com>
2022-09-25 10:15:57 -0400

    Added /tables route to redirect user to dashboard instead of Not Found

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -38,6 +38,9 @@ function Routes() {
       <Route exact={true} path="/reservations/:reservation_id/seat">
         <SeatReservation />
       </Route>
+      <Route exact={true} path="/tables">
+        <Redirect to={"/dashboard"} />
+      </Route>
       <Route exact={true} path="/tables/new">
         <CreateTable />
       </Route>
