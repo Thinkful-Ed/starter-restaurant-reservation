b64a29e596ce0bf1f71f88b0a23ad26cd026be31
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-10-19 11:24:04 -0700

    created reservations.service

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,10 +1,11 @@
+const reservationsService = require("./reservations.service");
 /**
  * List handler for reservation resources
  */
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const data = await reservationsService.list();
+  res.json({ data });
 }
 
 module.exports = {
