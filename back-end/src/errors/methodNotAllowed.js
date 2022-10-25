7d0638838fa168b88614955b74c8c0692d7b23d0
Tasha Hutnick <ahutnick@sas.upenn.edu>
2022-10-25 18:41:59 -0400

    add method not allowed error

---
+++ back-end/src/errors/methodNotAllowed.js
@@ -0,0 +1,8 @@
+function methodNotAllowed(req, res, next) {
+    next({
+        status: 405,
+        message: `${req.method} not allowed for ${req.originalUrl}`,
+    });
+}
+
+module.exports = methodNotAllowed;
