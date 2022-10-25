f8aaee2f1bbb6a6dd6d053797223c43d4e17d1c9
Tasha Hutnick <ahutnick@sas.upenn.edu>
2022-10-25 18:42:29 -0400

    add reservations list function

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,20 @@
+const services = require("./reservations.services");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 /**
  * List handler for reservation resources
  */
+
+function getDate() {
+  const today = new Date();
+  return `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+}
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const data = await services.list(req.query.date ? req.query.date : getDate());
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,7 +6,10 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list);
+router.route("/")
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 module.exports = router;
