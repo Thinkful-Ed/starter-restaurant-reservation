136b2107100af393532a131bd9a50ad40e928867
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-10-26 19:00:16 -0400

    starting changes

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,6 +1,15 @@
 const knex = require('../db/connection');
 
-
+async function list(date) {
+  if (date) {
+    return knex('reservations')
+      .select('*')
+      // .where({ date: })
+  } else {
+    return knex('reservations')
+      .select('*')
+  }
+}
 
 module.exports = {
   list,
