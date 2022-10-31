f93a668fb2aacf5ccd1403323885513197cb020b
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-10-30 18:42:21 -0700

    created tables serv, contr, router

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
