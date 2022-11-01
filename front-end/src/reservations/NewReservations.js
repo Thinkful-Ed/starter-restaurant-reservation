9dba676b0ef6df2fafd31449783d33990156d601
Xander Juneau <xanderjuneau@gmail.com>
2022-11-01 13:31:02 -0400

    added ErrorAlert component to reservations/new route, all tests for US02 e2e passing

--- front-end/src/reservations/NewReservations.js
+++ front-end/src/reservations/NewReservations.js
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
-import ReservationForm from './ReservationForm';
+import React, { useState } from "react";
+import ReservationForm from "./ReservationForm";
 import { useHistory } from "react-router";
-import { createReservation } from '../utils/api';
-import { formatAsDate } from '../utils/date-time';
-
+import { createReservation } from "../utils/api";
+import { formatAsDate } from "../utils/date-time";
+import ErrorAlert from "../layout/ErrorAlert";
 function NewReservations() {
-    const initReservation = {
-        first_name: "",
-        last_name: "",
-        mobile_number: "",
-        reservation_date: "",
-        reservation_time: "",
-        people: "",
-      };
-      
-      const [reservation, setReservation] = useState(initReservation);
-      const history = useHistory();
+  const initReservation = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  };
 
-      const submitHandler = (event) => {
-        event.preventDefault()
+  const [reservation, setReservation] = useState(initReservation);
+  const [reservationsError, setReservationsError] = useState(null);
+  const history = useHistory();
 
-        const abortController = new AbortController()
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-        createReservation(reservation, abortController.signal)
-            .then((createdReservation)=>{
-                history.push(`/dashboard?date=${formatAsDate(reservation.reservation_date)}`)
-            })
+    const abortController = new AbortController();
 
-        return ()=> abortController.signal
-      }
-      const cancelHandler = () => {
-        history.goBack()
-      }
-    return ( 
-        <div>
-            <h2>New Reservation:</h2>
-            <ReservationForm
-                reservation={reservation}
-                setReservation={setReservation}
-                submitHandler={submitHandler}
-                cancelHandler={cancelHandler}
-            />
-        </div>
-     );
+    createReservation(reservation, abortController.signal)
+      .then((createdReservation) => history.push(`/dashboard?date=${formatAsDate(reservation.reservation_date)}`))
+      .catch(setReservationsError)
+
+    return () => abortController.signal;
+  };
+  const cancelHandler = () => {
+    history.goBack();
+  };
+  return (
+    <div>
+      <h2>New Reservation:</h2>
+      <ErrorAlert error={reservationsError} />
+      <ReservationForm
+        reservation={reservation}
+        setReservation={setReservation}
+        submitHandler={submitHandler}
+        cancelHandler={cancelHandler}
+      />
+    </div>
+  );
 }
 
 export default NewReservations;
