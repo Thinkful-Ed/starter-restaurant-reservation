02addf0e832f8988d07ad1bc96d03012baa1cab1
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-11-01 15:01:30 -0700

    created update midware+func; US-04 backend passing

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
+router.route("/:table_id/seat").put(controller.update).all(methodNotAllowed);
+
 router
   .route("/")
   .get(controller.list)
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -11,7 +11,22 @@ function create(table) {
     .then((createRecords) => createRecords[0]);
 }
 
+function read(table_id) {
+  return knex("tables").select("*").where({ table_id }).first();
+}
+
+// /PUT to /tables/:table_id/seat/ to save table assignment
+function update(updatedRes, tableId) {
+  return knex("tables")
+    .select("*")
+    .where({ table_id: tableId })
+    .update(updatedRes, "*")
+    .then((updatedRecord) => updatedRecord[0]);
+}
+
 module.exports = {
   list,
   create,
+  read,
+  update,
 };
