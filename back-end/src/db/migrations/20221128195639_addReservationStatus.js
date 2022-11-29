91ad9d7fc1bae98195090225f64b27ac78df2fc4
Tasha Hutnick <ahutnick@sas.upenn.edu>
2022-11-28 23:13:30 -0500

    add reservation status migration

---
+++ back-end/src/db/migrations/20221128195639_addReservationStatus.js
@@ -0,0 +1,14 @@
+const knex = require("../connection");
+
+exports.up = function(knex) {
+  return knex.schema.alterTable("reservations", (table) => {
+    table.string("status").defaultTo("booked");
+  })
+};
+
+exports.down = function(knex) {
+    return knex.schema.alterTable("reservations", (table) => {
+        table.dropColumn("status");
+    })
+  
+};
