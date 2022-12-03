e93b55fb717a1c49f15f3a65afc0421a8d7a680e
Alexandra Reynaldo <catalpacapsicum@gmail.com>
2022-12-02 18:08:25 -0700

    migrations and seeds working

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -2,11 +2,11 @@ exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
     table.string("first_name").notNullable();
-    table.string("last_name").notNullable();
-    table.string("mobile_number").notNullable();
-    table.date("date_of_reservation").notNullable();
-    table.time("time_of_reservation").notNullable();
-    table.integer("people").unsigned().notNullable();
+    table.string("last_name", null).notNullable();
+    table.string("mobile_number", null).notNullable();
+    table.date("reservation_date").notNullable();
+    table.time("reservation_time").notNullable();
+    table.integer("people", null).unsigned().notNullable();
     table.timestamps(true, true);
   });
 };
@@ -14,3 +14,20 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
+
+// exports.up = function (knex) {
+//   return knex.schema.createTable("reservations", (table) => {
+//     table.increments("reservation_id").primary();
+//     table.string("first_name").notNullable();
+//     table.string("last_name").notNullable();
+//     table.string("mobile_number").notNullable();
+//     table.date("date_of_reservation").notNullable();
+//     table.time("time_of_reservation").notNullable();
+//     table.integer("people").unsigned().notNullable();
+//     table.timestamps(true, true);
+//   });
+// };
+
+// exports.down = function (knex) {
+//   return knex.schema.dropTable("reservations");
+// };
