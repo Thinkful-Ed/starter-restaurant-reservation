10a5a882b599d757e1f6d296735074101b038439
Jaden Monroe <jaden1@MacBook-Pro.lan>
2022-12-13 15:53:10 -0800

    gave functionality to most of user story one it still needs to pass a couple of tests

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,16 +1,60 @@
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 const service = require("./reservationsService")
+const hasProperties = require("../errors/hasProperties");
+const P = require("pino");
 /**
  * List handler for reservation resources
  */
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+]
 
+const hasRequiredProperties = hasProperties(...VALID_PROPERTIES)
+
+function validateDate(req, res, next) {
+  const { data = {} } = req.body;
+    if (!Date.parse(data.reservation_date)){
+    next({
+        status:400,
+        message: `reservation_date is invalid`
+      });
+    };
+  next();
+}
+
+function validateTime(req, res, next ){
+  const time = req.body.data.reservation_time;
+  if (!time.match(/^(\d{1,2}):(\d{2})([ap]m)?$/)){
+   return next({
+      status:400,
+      message:"reservation_time must be in hh,mm,ss"
+    });
+  }
+  next();
+};
+
+function validatePeople(req, res, next){
+  const people = req.body.data.people;
+  if (typeof(people) !== "number"){
+    next({
+      status:400,
+      message: `people is invalid`
+    });
+  }
+  next()
+};
 
 async function list(req, res) {
   console.log(req.params)
   res.json({
     data: await service.list()
   });
-}
+};
 
 async function create(req, res,) {
   const data = await service.create(req.body.data);
@@ -19,6 +63,13 @@ async function create(req, res,) {
 
 module.exports = {
   list,
-  create,
+  create: [
+    hasRequiredProperties,
+    validateDate,
+    validateTime,
+    validatePeople,
+    asyncErrorBoundary(create),
+  ],
+
 
 };
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -7,10 +7,9 @@ const MethodNotAllowed = require("../errors/MethodNotAllowed")
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/new")
-    .post(controller.create)
 
 router.route("/")
-    .get(controller.list);
-
+    .get(controller.list)
+    .post(controller.create)
+    .all(MethodNotAllowed)
 module.exports = router;
