d222ab7bcff1ee186539c7115848f8bd45dc2c65
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-23 18:22:44 -0500

    minor file updates

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.date("reservation_date").notNullable();
+    table.time("reservation_time").notNullable();
+    table.integer("people").notNullable();
     table.timestamps(true, true);
   });
 };
