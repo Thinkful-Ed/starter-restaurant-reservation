67a8bc8320587a5006ed0f4a3a1d0c5adf9cf022
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-26 11:52:06 -0500

    backend link gitignore update

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -6,7 +6,7 @@ import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+  process.env.REACT_APP_API_BASE_URL || "https://capstonereservationbackend.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
