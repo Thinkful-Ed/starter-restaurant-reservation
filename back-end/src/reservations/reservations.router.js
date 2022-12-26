126e9dcb2a1668c4df5f6c31bb5f437fbaf2be45
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-26 10:33:18 -0500

    updates to migration and seed data

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -4,9 +4,14 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
+const router = require("express").Router({mergeParams:true});
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+//Use CORS
+router.use(cors());
 
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:reservationId").get(controller.read).post(controller.update).all(methodNotAllowed);
+router.route("/new").post(controller.create);
 
 module.exports = router;
