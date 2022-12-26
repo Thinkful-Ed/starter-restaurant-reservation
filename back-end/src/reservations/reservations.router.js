5a2c84b8c77b9c16d2d233212ed575d9c31b912b
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-26 10:46:51 -0500

    Quick fix to comment out post until created

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -12,7 +12,11 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 router.use(cors());
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:reservationId").get(controller.read).post(controller.update).all(methodNotAllowed);
-router.route("/new").post(controller.create);
+router.route("/:reservationId").get(controller.read).all(methodNotAllowed);
+//update with post once I create them
+//router.route("/:reservationId").get(controller.read).post(controller.update).all(methodNotAllowed);
+
+//update with post once I create them
+//router.route("/new").post(controller.create);
 
 module.exports = router;
