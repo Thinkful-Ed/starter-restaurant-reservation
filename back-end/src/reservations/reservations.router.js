61d98100871bf18d238eaaa8aaf4de8d71a354ce
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-27 09:13:56 -0500

    post update

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -11,11 +11,11 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 //Use CORS
 //router.use(cors());
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 //router.route("/:reservationId").get(controller.read).all(methodNotAllowed);
 router.route("/:reservationId").get(controller.read).put(controller.update).all(methodNotAllowed);
 
 
-router.route("/new").post(controller.create).all(methodNotAllowed);
+//router.route("/new").post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
