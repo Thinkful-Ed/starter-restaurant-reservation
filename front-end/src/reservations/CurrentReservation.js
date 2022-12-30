6906fa1bfa8013f7e457259f319d3b473a8e0682
jumroon <jumroon.rada@gmail.com>
2022-12-30 04:32:19 -0600

    display select with list of tables

--- front-end/src/reservations/CurrentReservation.js
+++ front-end/src/reservations/CurrentReservation.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { listTables } from "../utils/api";
 
 function CurrentReservation() {
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  useEffect(loadTables, []);
+
+  function loadTables() {
+    const abortController = new AbortController();
+    setTablesError(null);
+    listTables(abortController.signal).then(setTables).catch(setTablesError);
+    return () => abortController.abort();
+  }
+
+  //get current reservation by reservationId
   const { reservationId } = useParams();
-  return <div>I AM RESERVATION {reservationId}</div>;
+  return (
+    <div>
+      I AM RESERVATION {reservationId}
+      <form>
+        <label htmlFor="table_id">Table</label>
+        <select name="table_id">
+          {tables.map((table) => (
+            <option>
+              {table.table_name} - {table.capacity}
+            </option>
+          ))}
+        </select>
+      </form>
+    </div>
+  );
 }
 
 export default CurrentReservation;
