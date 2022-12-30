2591aa49041f07efe829ab39298d0ce6ee3f0178
Lee <57182164+LeeJay124@users.noreply.github.com>
2022-12-30 16:07:11 -0500

    tables routing, migration, and seeding

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -5,10 +5,10 @@
  */
 
 const router = require("express").Router({mergeParams:true});
-const controller = require("./reservations.controller");
+const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
-router.rout("/:tableId/seat").put(controller.update).all(methodNotAllowed);
+router.route("/:tableId/seat").put(controller.update).all(methodNotAllowed);
 
 module.exports = router;
