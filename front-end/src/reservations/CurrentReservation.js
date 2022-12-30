e937d9e5a8c1031ad24da7d614c91a1cd18b7b36
jumroon <jumroon.rada@gmail.com>
2022-12-30 04:48:42 -0600

    current reservation page
    displays options for tables and tableId

--- front-end/src/reservations/CurrentReservation.js
+++ front-end/src/reservations/CurrentReservation.js
@@ -2,9 +2,19 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { listTables } from "../utils/api";
 
+const INITIAL_FORM_DATA = {
+  table_id: "",
+};
+
 function CurrentReservation() {
   const [tables, setTables] = useState([]);
   const [tablesError, setTablesError] = useState(null);
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+
+  const handleChange = (event) => {
+    const { target } = event;
+    setFormData({ ...formData, [target.id]: target.value });
+  };
 
   useEffect(loadTables, []);
 
@@ -20,11 +30,16 @@ function CurrentReservation() {
   return (
     <div>
       I AM RESERVATION {reservationId}
+      <div>{JSON.stringify(formData)}</div>
       <form>
         <label htmlFor="table_id">Table</label>
-        <select name="table_id">
+        <select
+          name="table_id"
+          id="table_id"
+          value={formData.table_id}
+          onChange={handleChange}>
           {tables.map((table) => (
-            <option>
+            <option value={table.table_id}>
               {table.table_name} - {table.capacity}
             </option>
           ))}
