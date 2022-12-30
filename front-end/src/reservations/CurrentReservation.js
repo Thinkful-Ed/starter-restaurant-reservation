e01cecdbbf29115ad8dfe8dca72e7c00947d76cd
jumroon <jumroon.rada@gmail.com>
2022-12-30 05:26:29 -0600

    created placeholder select, and made put request to setTableToOccupied API

--- front-end/src/reservations/CurrentReservation.js
+++ front-end/src/reservations/CurrentReservation.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { listTables } from "../utils/api";
+import { listTables, setTableToOccupied } from "../utils/api";
 
 const INITIAL_FORM_DATA = {
   table_id: "",
@@ -10,13 +10,14 @@ function CurrentReservation() {
   const [tables, setTables] = useState([]);
   const [tablesError, setTablesError] = useState(null);
   const [formData, setFormData] = useState(INITIAL_FORM_DATA);
+  const { reservationId } = useParams();
+  const [reservationsError, setReservationsError] = useState(null);
+  useEffect(loadTables, []);
 
   const handleChange = (event) => {
     const { target } = event;
     setFormData({ ...formData, [target.id]: target.value });
   };
-
-  useEffect(loadTables, []);
 
   function loadTables() {
     const abortController = new AbortController();
@@ -25,25 +26,44 @@ function CurrentReservation() {
     return () => abortController.abort();
   }
 
-  //get current reservation by reservationId
-  const { reservationId } = useParams();
+  async function onSubmit(event) {
+    event.preventDefault();
+    const abortController = new AbortController();
+
+    try {
+      console.log("formData", formData);
+      await setTableToOccupied(
+        formData.table_id,
+        reservationId,
+        abortController.signal
+      );
+      // history.push(`/dashboard?date=${formData.reservation_date}`);
+    } catch (error) {
+      setReservationsError(error);
+    }
+  }
+
   return (
     <div>
       I AM RESERVATION {reservationId}
       <div>{JSON.stringify(formData)}</div>
-      <form>
+      <form onSubmit={onSubmit}>
         <label htmlFor="table_id">Table</label>
         <select
           name="table_id"
           id="table_id"
           value={formData.table_id}
           onChange={handleChange}>
+          <option value="" disabled>
+            Select a table
+          </option>
           {tables.map((table) => (
             <option value={table.table_id}>
               {table.table_name} - {table.capacity}
             </option>
           ))}
         </select>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
