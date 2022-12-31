7e16df8217ab1c3502d7f3d9f14337cad249a27c
jumroon <jumroon.rada@gmail.com>
2022-12-31 04:55:23 -0600

    cancel button cancels a reservation with confirmation window

--- front-end/src/reservations/ReservationsList.js
+++ front-end/src/reservations/ReservationsList.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { setReservationStatus } from "../utils/api";
+import { useHistory } from "react-router-dom";
 
 function ReservationsList({ reservations }) {
+  const history = useHistory();
+
+  const handleCancel = async (reservation_id) => {
+    if (
+      window.confirm(
+        "Do you want to cancel this reservation? This cannot be undone."
+      )
+    ) {
+      await setReservationStatus(reservation_id, "cancelled");
+      history.go(0);
+    }
+  };
+
   return (
     <div>
       <div className="d-md-flex mb-3">
@@ -29,6 +44,11 @@ function ReservationsList({ reservations }) {
                 <a href={`/reservations/${reservation.reservation_id}/edit`}>
                   <button>Edit</button>
                 </a>
+                <button
+                  data-reservation-id-cancel={reservation.reservation_id}
+                  onClick={() => handleCancel(reservation.reservation_id)}>
+                  Cancel
+                </button>
               </div>
             </div>
           );
