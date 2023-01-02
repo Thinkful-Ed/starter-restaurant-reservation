7f7de3708682a18fc6920756c592d36a263cbe2d
jumroon <jumroon.rada@gmail.com>
2023-01-02 08:30:46 -0600

    added not nullable fields into the backend/database

--- back-end/src/db/migrations/20221223223513_tables.js
+++ back-end/src/db/migrations/20221223223513_tables.js
@@ -1,14 +1,14 @@
 exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
-    table.increments("table_id").primary();
+    table.increments("table_id").primary().notNullable();
     table.integer("reservation_id");
     table
       .foreign("reservation_id")
       .references("reservation_id")
       .inTable("reservations")
       .onDelete("CASCADE");
-    table.string("table_name");
-    table.integer("capacity");
+    table.string("table_name").notNullable();
+    table.integer("capacity").notNullable();
     table.timestamps(true, true); //creates the created_at and updated_at columns
   });
 };
