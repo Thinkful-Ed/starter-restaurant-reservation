63b2efb14c5a89396bc61fbc3e8ad3d8c0ac676a
Lee <57182164+LeeJay124@users.noreply.github.com>
2023-01-02 11:14:18 -0500

    link tweak

--- front-end/src/reservations/Reservation.js
+++ front-end/src/reservations/Reservation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Reservation({reservation, handleReservationCancel}){
 
@@ -16,12 +17,12 @@ function Reservation({reservation, handleReservationCancel}){
             <td data-reservation-id-status={`${reservation.reservation_id}`}>{reservation.status}</td>
 
             <td> 
-            {reservation.status === "booked" ? <a href={`/reservations/` + reservation.reservation_id + `/edit`} className="mt-2 mr-2">
+            {reservation.status === "booked" ? <Link to={`/reservations/${reservation.reservation_id}/edit`} className="mt-2 mr-2">
               <button className="btn btn-secondary">Edit</button>
-              </a>: ""}
-              {reservation.status === "booked" ? <a href={`/reservations/` + reservation.reservation_id + `/seat`} className="mt-2 mr-2">
+              </Link>: ""}
+              {reservation.status === "booked" ? <Link to={`/reservations/${reservation.reservation_id}/seat`}  className="mt-2 mr-2">
               <button className="btn btn-info">Seat</button>
-              </a>: ""}
+              </Link>: ""}
               <button data-reservation-id-cancel={`${reservation.reservation_id}`} className="btn btn-danger" onClick={()=> handleReservationCancel(reservation.reservation_id)}>Cancel</button>
               </td>
        
