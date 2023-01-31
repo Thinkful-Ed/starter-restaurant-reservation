02472a277da1afe8321a9418b9aa1a3b45fdfff2
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-01-31 09:02:47 -0500

    Update 20210123122225_createReservationsTable.js

---
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("reservations", (table) => {
+    table.increments("reservation_id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.string("reservation_date").notNullable();
+    table.string("reservation_time").notNullable();
+    table.integer("people").notNullable()
+    table.string("status").notNullable().defaultTo("pending")
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("reservations");
+};
