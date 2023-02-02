d2ceba09e95e4f348d8dd6082e9f63aef4e2eac8
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-02-02 13:44:54 -0500

    US-7-backend

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -41,9 +41,20 @@ function update(updatedRes) {
         .then((updated) => updated[0])
 }
 
+function search(mobile_number) {
+    return knex("reservations")
+        .whereRaw(
+            "translate(mobile_number, '() -', '') like ?",
+            `%${mobile_number.replace(/\D/g, "")}%`
+        )
+       .orderBy("reservation_date") 
+}
+
+
 module.exports = {
     list,
     create,
     read,
     update,
+    search,
 }
