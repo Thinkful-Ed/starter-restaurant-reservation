235830313e34ff3cc5607124338aae170010e997
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-02-02 19:48:41 -0500

    Validation files cleanup

--- back-end/src/utils/validateReservationInput.js
+++ back-end/src/utils/validateReservationInput.js
@@ -1,86 +1,61 @@
 function validType(){
-    return function validateTypes(req, res, next){
+    return function validateTypes(req, _res, next){
         const { data } = req.body;
         // regular expression to match required date format
         const reDate = /^(\d{4})-(\d{1,2})-(\d{1,2})/;
         // regular expression to match required time format
         const reTime = /^\d{1,2}:\d{2}([ap]m)?$/;
         
-        // if(typeof(data.people) !== 'number'){
-        // next({ status:400, message: 'people must be a number.'})
-        // };
-    
-        // if(!data.reservation_date.match(reDate)) {
-        // next({ status:400, message: 'Please enter a valid reservation_date.'})
-        // };
-    
-        // if(!data.reservation_time.match(reTime)) {
-        // next({ status:400, message: 'Please enter a valid reservation_time.'})
-        // };
+        const reservationDate = new Date(data.reservation_date);
 
-        let errorField = ''
-        switch (true) {
-            case typeof(data.people) !== 'number':
-                errorField = 'people';
-                break;
-            case !data.reservation_date.match(reDate):
-                errorField = 'reservation_date';
-                break;
-            case !data.reservation_time.match(reTime):
-                errorField = 'reservation_time';
-                break;
-            default:
-                break;            
-        }
-        if(errorField){
-            next({ status: 400, message: `${errorField} is invalid.`})
-        }
+        const todaysDate = new Date(Date.now());
 
-        let reservationDay = new Date(data.reservation_date)
+        const resYear = reservationDate.getUTCFullYear();
+        const resMonth = reservationDate.getUTCMonth();
+        const resDay = reservationDate.getUTCDate();
 
-        if(reservationDay.getUTCDay() === 2){
-            next({ status:400, message: 'Sorry, we are closed on Tuesdays'})
-        }
-
-        const todayDateLong = Date.now();
-        const todayDate = new Date(todayDateLong);      //Wed Feb 01 2023 18:42:11 GMT-0500 (Eastern Standard Time)
-
-
-        const resYear = reservationDay.getUTCFullYear();
-        const resMonth = reservationDay.getUTCMonth();
-        const resDay = reservationDay.getUTCDate();
-
-        const thisYear = todayDate.getUTCFullYear();
-        const thisMonth = todayDate.getUTCMonth();
-        const thisDay = todayDate.getUTCDate();
-    
-
-        if(thisYear > resYear){
-            next({ status:400, message: 'You can only make reservations for the future.'})
-        } 
-        if(thisYear == resYear && thisMonth > resMonth){
-            next({ status:400, message: 'You can only make reservations for the future.'})
-        }
-        if(thisYear == resYear && thisDay > resDay){
-            next({ status:400, message: 'You can only make reservations for the future.'})
-        }
-
+        const thisYear = todaysDate.getUTCFullYear();
+        const thisMonth = todaysDate.getUTCMonth();
+        const thisDay = todaysDate.getUTCDate();
 
         const reservationTime = data.reservation_time;
         const reservationTimeHours = reservationTime.slice(0,2);
-        const reservationTimeMinutes = reservationTime.slice(3,5)
-       
-    
-        if(reservationTimeMinutes <= 30){
-            if(reservationTimeHours <= 10){
-                next({ status:400, message: 'Invalid time'})
-            }
+        const reservationTimeMinutes = reservationTime.slice(3,5);
+        
+        let errorMessage = ''
+        switch (true) {
+            case typeof(data.people) !== 'number':
+                errorMessage = 'people must be a number.';
+                break;
+            case !data.reservation_date.match(reDate):
+                errorMessage = 'reservation_date is an invalid format.  Please use YYYY:MM:DD format.';
+                break;
+            case !data.reservation_time.match(reTime):
+                errorMessage = 'reservation_time is an invalid format.';
+                break;
+            case reservationDate.getUTCDay() === 2:
+                errorMessage = 'Sorry, we are closed on Tuesdays.';
+                break;
+            case thisYear > resYear:
+                errorMessage = 'You can only make reservations for the future.';
+                break;
+            case thisYear == resYear && thisMonth > resMonth:
+                errorMessage = 'You can only make reservations for the future.';
+                break;
+            case thisYear == resYear && thisDay > resDay:
+                errorMessage = 'You can only make reservations for the future.';
+                break;
+            case reservationTimeMinutes <= 30 && reservationTimeHours <= 10:
+                errorMessage = 'Invalid time.';
+                break;
+            case reservationTimeMinutes >= 30 && reservationTimeHours >= 21:
+                errorMessage = 'Invalid time.'      
+            default:
+                break;            
         }
 
-        if(reservationTimeMinutes >= 30){
-            if(reservationTimeHours >= 21){
-                next({ status:400, message: 'Invalid time'})
-            }
+        if(errorMessage){
+            next({ status: 400, message: `${errorMessage}`})
         }
 
         next();
