01a0e85533aa688dda622f8127c222ee76c8d26d
Katie Gaines <klgaines18@gmail.com>
2023-04-21 06:01:55 -0400

    update list function to grab table data

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,14 @@
+const reservationsService = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+async function list(req, res, next) {
+  const data = await reservationsService.list();
+  res.json({ data });
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
 };
