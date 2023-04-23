3f58bc47d08375eaa1adf52d39b40978d1f61d05
Katie Gaines <klgaines18@gmail.com>
2023-04-23 09:51:11 -0400

    add create function

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,13 +1,21 @@
 const knex = require("../db/connection");
 
 function list() {
-  return knex("reservations").select("*");
+  return knex("reservations").select("*").orderBy("reservation_time");
 }
 
 function listDate(date) {
   return knex("reservations")
     .select("*")
     .where({ reservation_date: date })
+    .orderBy("reservation_time")
+}
+
+function read(reservationId) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_id: reservationId })
+    .first();
 }
 
 function create(newReservation) {
@@ -15,8 +23,14 @@ function create(newReservation) {
     .insert(newReservation)
 }
 
+function destroy(reservation_id) {
+  return knex("reservations").where({ reservation_id }).del();
+}
+
 module.exports = {
   list,
   listDate,
+  read,
   create,
+  delete: destroy,
 }
