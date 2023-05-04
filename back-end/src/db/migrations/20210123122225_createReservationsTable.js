2cf45426ac7a3a15591b1937de9feb396d661434
BradleyHollis <bhollis@dev-10.com>
2023-05-04 19:05:24 -0400

    Added required dependencies. Setup database connections and seeded database.

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name");
+    table.string("last_name");
+    table.string("mobile_number");
+    table.date("reservation_date");
+    table.time("reservation_time");
+    table.integer("people");
     table.timestamps(true, true);
   });
 };
@@ -8,3 +14,4 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
+
