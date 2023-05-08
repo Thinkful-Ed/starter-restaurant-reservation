d90defa66f1e2b42cf5616b08e0784da2973c95a
Hanna Barney <hanna.barney@icloud.com>
2023-05-07 22:15:03 -0400

    updates recovered branch

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,6 +1,9 @@
-const reservations = require('./00-reservations.json')
+const reservations = require("./00-reservations.json");
+
 exports.seed = function (knex) {
   return knex
     .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-    .then(() => knex('reservations').insert(reservations))
+    .then(function () {
+      return knex("reservations").insert(reservations);
+    });
 };
