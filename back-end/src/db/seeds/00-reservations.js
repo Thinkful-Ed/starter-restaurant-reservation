360de9c4553323268b2b89b864d048493befcdd3
BradleyHollis <bhollis@dev-10.com>
2023-05-09 17:58:48 -0400

    Added another test branch

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,6 +1,7 @@
 const reservations = require('./00-reservations.json');
 
 // Commenting this line
+// Commenting this line too
 exports.seed = function (knex) {
   return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
   .then(function (){
