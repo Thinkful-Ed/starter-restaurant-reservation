0e368e3c98262fc560b175fc57ac42f80d0aeef8
BradleyHollis <bhollis@dev-10.com>
2023-05-09 15:05:55 -0400

    Lists raw json data for reservations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,10 +1,10 @@
 /**
  * List handler for reservation resources
  */
+const reservationsService = require('./reservations.service');
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  res.json({ data: await reservationsService.list() });
 }
 
 module.exports = {
