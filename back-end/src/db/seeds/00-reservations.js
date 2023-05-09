c906f4d46f02530e6d8baa15bb6fccc61cc5dd7a
BradleyHollis <bhollis@dev-10.com>
2023-05-09 15:43:30 -0400

    Test

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,5 +1,6 @@
 const reservations = require('./00-reservations.json');
 
+// Adding comment
 exports.seed = function (knex) {
   return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
   .then(function (){
