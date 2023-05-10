4a190e90ebf5a0b9c4f4f0b85a0873ded9799dbc
BradleyHollis <bhollis@dev-10.com>
2023-05-10 17:42:28 -0400

    List functions added to service and controller

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,11 +1,18 @@
 /**
  * List handler for reservation resources
  */
+const reservationsService = require('./reservations.service');
+
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const date = req.query.date;
+  const mobile_number = req.query.mobile_number;
+  const data = await (date
+    ? reservationsService.list(date)
+    : reservationsService.search(mobile_number));
+  res.json({ data });
 }
+
+
 
 module.exports = {
   list,
