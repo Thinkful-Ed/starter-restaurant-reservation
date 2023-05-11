7c5823e29b18e296cda055fe94e58ab81815466f
BradleyHollis <bhollis@dev-10.com>
2023-05-11 16:00:38 -0400

    Table Read operation added to service

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,7 +1,21 @@
 const tablesService = require('./tables.service');
+const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
 async function list(req, res){
     res.json({ data: await tablesService.list() })
+}
+
+async function tableExists(req, res, next){
+    const { tableId } = req.params; 
+    const table = await tablesService.read(tableId);
+    if(table){
+        res.locals.table = table;
+        return next();
+    }
+    next({
+        status: 404,
+        message: `Table ${tableId} cannot be found.`
+    })
 }
 
 module.exports = {
