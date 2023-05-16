57a6b6a56a8e0dd579955dd3e413e82180a1f0d8
BradleyHollis <bhollis@dev-10.com>
2023-05-16 14:04:31 -0400

    Listing Reservations on front end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -14,8 +14,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.options("*", cors());
-
 app.use("/reservations", reservationsRouter);
 app.use('/tables', tablesRouter);
 
