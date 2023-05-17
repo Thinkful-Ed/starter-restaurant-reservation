1dbdb2296f0083a9b69ce72a6a7e15caab5670df
BradleyHollis <bhollis@dev-10.com>
2023-05-17 16:52:54 -0400

    Reverting connection file back to original state

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,11 +1,6 @@
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
-
-if(process.env.NODE_ENV != 'test') {
-    knex.migrate.latest([config])
-    knex.seed.run([config])
-}
 
 module.exports = knex;
