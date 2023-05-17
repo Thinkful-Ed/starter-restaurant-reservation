b728c9b8e8c77660d866049f57c6936844ca8930
BradleyHollis <bhollis@dev-10.com>
2023-05-17 02:51:14 -0400

    Seeding

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -2,8 +2,10 @@ const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
+
 if(process.env.NODE_ENV != 'test') {
     knex.migrate.latest([config])
+    knex.seed.run([config])
 }
 
 module.exports = knex;
