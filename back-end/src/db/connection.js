76fadbf9352bf33dd7e6c660660ec4bb406ad38a
BradleyHollis <bhollis@dev-10.com>
2023-05-17 02:31:52 -0400

    Edits for deployment

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -2,4 +2,8 @@ const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
+if(process.env.NODE_ENV != 'test') {
+    knex.migrate.latest([config])
+}
+
 module.exports = knex;
