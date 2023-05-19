0c85319987f9e3f35d257f911e052ef0c2bd4389
BradleyHollis <bhollis@dev-10.com>
2023-05-19 18:17:32 -0400

    Toggling configurations

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
