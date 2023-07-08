019510ab9984558f6ffa92676e7846c86740ad1d
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-07-08 00:02:43 -0400

    add form to create tables and create component to render the form

--- front-end/src/tables/TableCreate.js
+++ front-end/src/tables/TableCreate.js
@@ -1,9 +1,64 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+
+//component imports
 import ErrorAlert from "../layout/ErrorAlert";
 import TableForm from "./TableForm";
 
-function TableCreate() {}
+//utility functions
+import { createTable } from "../utils/api";
+
+//Handle the creation of a table by rendering the imported table form with the create table utility to input table details, submit the form, and handle any errors
+function TableCreate() {
+	const history = useHistory();
+	const initialFormState = {
+		table_name: " ",
+		capacity: " ",
+	};
+	const [formData, setFormData] = useState({ ...initialFormState });
+	const [error, setError] = useState(null);
+	const cancelHandler = () => {
+		history.goBack();
+	};
+	//handle form submission; prevent default; error state set to null
+	const submitHandler = async (event) => {
+		event.preventDefault();
+		setError(null);
+		//abort controller  is created in case of needing to cancel the API request
+		const abortController = new AbortController();
+		//convert capacity value to a number
+		formData.capacity = Number(formData.capacity);
+		//call createTable with formdata and aborController as parameters to create a table via an API request.
+		try {
+			await createTable(formData, abortController.signal);
+			history.push("/");
+		} catch (error) {
+			setError(error);
+		}
+		return () => abortController.abort();
+	};
+
+	//the change handler updates the formData state with the new value
+	const changeHandler = ({ target: { name, value } }) => {
+		setFormData((previousFormDAta) => ({
+			...previousFormData,
+			[name]: value,
+		}));
+	};
+	//render the component; display any error stored in the error state
+	return (
+		<main>
+			<ErrorAlert error={error} />
+			<TableForm
+				submitHandler={submitHandler}
+				cancelHandler={cancelHandler}
+				formData={formData}
+				changeHandler={changeHandler}
+			/>
+		</main>
+	);
+}
 
 export default TableCreate;
--- front-end/src/tables/TableForm.js
+++ front-end/src/tables/TableForm.js
@@ -2,6 +2,65 @@
 
 import React from "react";
 
-function TableForm({ submitHandler, cancelHandler, changeHandler, formData }) {}
+// To allow user to assign a reservation to a specific table and
+// so that user will then be able to know which tables are occupied/free,
+// provde a table creation form so that use can name/identify specific tables
+function TableForm({ submitHandler, cancelHandler, changeHandler, formData }) {
+	return (
+		<form onSubmit={changeHandler}>
+			<fieldset>
+				<h2 className="mb-3 pt-3">Create a New Table</h2>
+				<div className="mb-3">
+					<label
+						className="form-label"
+						htmlFor="table_name">
+						{" "}
+						Table Name
+					</label>
+					<input
+						className="form_contorl"
+						name="table_name"
+						id="table_name"
+						type="text"
+						value={formData.table_name}
+						maxLength="100"
+						minLength="2"
+						onChange={changeHandler}
+					/>
+				</div>
+				<div className="mb-3">
+					<label
+						className="form_label"
+						htmlFor="capacity">
+						Capacity
+					</label>
+					<input
+						className="form-control"
+						name="capacity"
+						id="capacity"
+						type="number"
+						required={true}
+						value={formData.capacity}
+						onChange={changeHandler}
+						min={1}
+					/>
+				</div>
+				<div>
+					<button
+						type="submit"
+						className="btn btn-primary">
+						Submit
+					</button>
+					<button
+						type="button"
+						className="btn btn-secondary"
+						onClick={cancelHandler}>
+						Cancel
+					</button>
+				</div>
+			</fieldset>
+		</form>
+	);
+}
 
 export default TableForm;
