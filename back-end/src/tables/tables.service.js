b4c7b1e590d6b8fbe06078b8ee6c5952d65f47ad
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-07-08 23:42:11 -0400

    added tables.service

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,3 +1,33 @@
 /** @format */
 
-import knex from "../db/connection";
+const knex = require("../db/connection");
+
+function create(newTable) {
+	return knex("tables")
+		.insert(newTable)
+		.returning("*")
+		.then((createdRecord) => createdRecord[0]);
+}
+
+function read(table_id) {
+	return knex("tables").select("*").where({ table_id }).first();
+}
+
+function update(updatedTable) {
+	return knex("tables")
+		.select("*")
+		.where({ table_id: updatedTable.table_id })
+		.update(updatedTable, "*")
+		.then((updatedRecords) => updatedRecords[0]);
+}
+
+function list() {
+	return knex("tables").select("*").orderBy("table_name");
+}
+
+module.exports = {
+	create,
+	read,
+	update,
+	list,
+};
