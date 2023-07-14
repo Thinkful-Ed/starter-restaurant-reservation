f84980f5f38c957b49f9e294c3ffa2caea139782
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-07-14 17:39:03 -0400

    modify pack.json files

--- front-end/src/layout/Footer.js
+++ front-end/src/layout/Footer.js
@@ -6,7 +6,6 @@ const Footer = () => {
 	return (
 		<div className="container">
 			<div className="col justify-content-between py-3 my-4 border-top">
-				<p className="row mb-0 text-muted">© 2023 Olga Yudkin.</p>
 				<p className="row mb-0 text-muted">
 					<a href="#top">Back to top</a>
 				</p>
