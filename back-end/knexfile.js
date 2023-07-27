91f126c94ff415ac509b185ac8bfcde909ac3334
Ashley Nguyen <ashleynguyen37@gmail.com>
2023-07-26 23:14:54 -0500

    troubleshooting to deploy server

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://qtgmuonf:GER6zp9YBk1OhltW02MVqchSbMusptIA@trumpet.db.elephantsql.com/qtgmuonf",
+  DATABASE_URL_DEVELOPMENT = "postgres://ufygojxu:7QHq2z5Np47mtUdj8d4hDGhgKw1j9EIv@trumpet.db.elephantsql.com/ufygojxu",
+  DATABASE_URL_TEST = "postgres://hagocssf:YErEj0kiPWwBsEnYcFMorr0gdHG3YcYe@trumpet.db.elephantsql.com/hagocssf",
+  DATABASE_URL_PREVIEW = "postgres://vaedkbjg:HZdxwggHj1y51L3FRNy8jDDPvWfA33Yk@trumpet.db.elephantsql.com/vaedkbjg",
   DEBUG,
 } = process.env;
 
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,13 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import NewReservation from "../reservations/NewReservation";
+import NewTable from "../tables/NewTable";
+import SeatReservation from "../reservations/SeatReservation";
+import EditReservation from "../reservations/EditReservation";
+import SearchByMobilePhone from "../reservations/SearchByMobilePhone";
 
 /**
  * Defines all the routes for the application.
@@ -23,6 +27,21 @@ function Routes() {
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
+      </Route>
+      <Route exact={true} path={"/reservations/new"}>
+        <NewReservation />
+      </Route>
+      <Route exact={true} path={"/tables/new"}>
+        <NewTable />
+      </Route>
+      <Route exact={true} path={`/reservations/:reservation_id/seat`}>
+        <SeatReservation />
+      </Route>
+      <Route exact={true} path={"/search"}>
+        <SearchByMobilePhone />
+      </Route>
+      <Route exact={true} path="/reservations/:reservation_id/edit">
+        <EditReservation />
       </Route>
       <Route>
         <NotFound />
