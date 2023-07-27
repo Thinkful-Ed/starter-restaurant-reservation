73caae774629f8e608b4f229952ad86e584addd8
Ashley Nguyen <ashleynguyen37@gmail.com>
2023-07-26 21:55:22 -0500

    migrations complete

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
     table.increments("reservation_id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("mobile_number").notNullable();
+    table.date("reservation_date").notNullable();
+    table.time("reservation_time").notNullable();
+    table.integer("people").unsigned().notNullable();
+    table.string("status").notNullable().defaultTo("booked");
     table.timestamps(true, true);
   });
 };
---
+++ back-end/src/db/migrations/20230726215359_createTablesTable.js
@@ -0,0 +1,19 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("tables", (table) => {
+      table.increments("table_id").primary();
+      table.string("table_name").notNullable();
+      table.integer("capacity").notNullable();
+      table.integer("reservation_id").unsigned();
+      table
+        .foreign("reservation_id")
+        .references("reservation_id")
+        .inTable("reservations")
+        .onDelete("CASCADE");
+      table.string("status").notNullable().defaultTo("free");
+      table.timestamps(true, true);
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("tables");
+  };
