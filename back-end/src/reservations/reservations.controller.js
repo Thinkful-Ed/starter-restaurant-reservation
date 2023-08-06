3037c942587d6e8281f636f0311ce96c1b65b487
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-08-06 08:10:14 -0400

    exported seed file

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -6,8 +6,13 @@ const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-  const response = await service.list()
+  const date = req.query.date;
+  const mobile_number = req.query.mobile_number;
 
+  const reservations = await service.list(date, mobile_number);
+
+  const response = reservations.filter((reservation) => reservation.status !== "finished");
+  
   res.json({ data: response})
 }
 
--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,8 +1,21 @@
 const knex =  require("../db/connection");
 
-function list() {
+function list(date, mobile_number) {
+    if (date) {
+        return knex("reservations")
+        .select("*")
+        .where({ reservation_date: date })
+        .orderBy("reservation_time", "asc");
+    }
+
+    if (mobile_number) {
+        return knext("reservations")
+        .select("*")
+        .where("mobile_number", "like", `${mobile_number}`);
+    }
+
     return knex("reservations")
-    .select("*")
+    .select("*");
 }
 
 module.exports = {
