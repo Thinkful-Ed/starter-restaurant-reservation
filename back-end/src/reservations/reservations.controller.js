1e828bd184dd6de7d0f8bc4045df71560ac88b52
sleon204 <sleon87@gmail.com>
2023-08-15 21:43:18 -0700

    Update reservations.controller.js
    converted list to an async function and incorporated reservations.service

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,24 @@
+const reservationsService = require('./reservations.service');
+
 /**
  * List handler for reservation resources
  */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+
+// list all reservations for one date only. (E.g. if the URL is `/dashboard?date=2035-12-30`
+//then send a GET to `/reservations?date=2035-12-30` to list the reservations for that date).
+//The date is defaulted to today, and the reservations are sorted by time.
+
+async function list(req, res, next) {
+	const { date } = req.query;
+
+	try {
+		const data = await reservationsService.list(date);
+		res.json({ data });
+	} catch (error) {
+		next(error);
+	}
 }
 
 module.exports = {
-  list,
+	list,
 };
