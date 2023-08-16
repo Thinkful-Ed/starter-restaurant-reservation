11e10dc6e47bdd2bcaa8448a4b83ac35916302cf
sleon204 <sleon87@gmail.com>
2023-08-16 12:33:34 -0700

    Create NewReservation.js
    default NewReservation function

---
+++ front-end/src/Reservations/NewReservation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReservationForm from "./ReservationForm";
+
+export default function NewReservation() {
+  return (
+    <div>
+      <ReservationForm />
+    </div>
+  );
+}
