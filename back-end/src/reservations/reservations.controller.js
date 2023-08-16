49b162ab1f55f7e8c21794d4af48ad28ec03bce0
sleon204 <sleon87@gmail.com>
2023-08-15 22:11:23 -0700

    Update reservations.controller.js
    removed comment

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,10 +5,6 @@ const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
  * List handler for reservation resources
  */
 
-// list all reservations for one date only. (E.g. if the URL is `/dashboard?date=2035-12-30`
-//then send a GET to `/reservations?date=2035-12-30` to list the reservations for that date).
-//The date is defaulted to today, and the reservations are sorted by time.
-
 async function list(req, res) {
   const reservations = await reservationsService.list();
   res.json({ data: reservations });
