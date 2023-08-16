480ad2a72cc7b85ae745d032fd7bba8824f19542
sleon204 <sleon87@gmail.com>
2023-08-15 22:10:16 -0700

    Update reservations.service.js
    sorted list by time

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,15 @@
 const knex = require('../db/connection');
 
 function list() {
-	return knex('reservations').select('*');
+  
+  return knex("reservations")
+    .select("*")
+    .orderBy("reservation_time");
 }
+
+
 
 module.exports = {
 	list,
+  
 };
