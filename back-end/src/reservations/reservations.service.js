1abecdf4969590e47a20ad0369b5fee660beefb9
sleon204 <sleon87@gmail.com>
2023-08-15 22:56:59 -0700

    Update reservations.service.js
    added create function and added date to list function

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,15 +1,23 @@
 const knex = require('../db/connection');
 
-function list() {
+function list(date) {
   
   return knex("reservations")
-    .select("*")
+    .where({ reservation_date: date })
     .orderBy("reservation_time");
+}
+
+function create(newReservation) {
+  return knex("reservations")
+    .insert(newReservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
 }
 
 
 
 module.exports = {
 	list,
+  create
   
 };
