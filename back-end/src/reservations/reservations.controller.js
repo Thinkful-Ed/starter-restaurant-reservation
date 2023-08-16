7a33e3a615c2a57c66bd249635882ab0b3df6c03
sleon204 <sleon87@gmail.com>
2023-08-15 21:45:43 -0700

    Update reservations.controller.js
    commented out date related code for now

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -9,10 +9,11 @@ const reservationsService = require('./reservations.service');
 //The date is defaulted to today, and the reservations are sorted by time.
 
 async function list(req, res, next) {
-	const { date } = req.query;
+	//const { date } = req.query;
 
 	try {
-		const data = await reservationsService.list(date);
+		//const data = await reservationsService.list(date);
+		const data = await reservationsService.list();
 		res.json({ data });
 	} catch (error) {
 		next(error);
