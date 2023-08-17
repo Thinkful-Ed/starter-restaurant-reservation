2e69fc010091fa6b945c0c6dce2ec4fe5c093650
TurnerSF <Turner_shreve@yahoo.com>
2023-08-16 19:26:12 -0700

    created migrations and seeding

---
+++ back-end/src/db/seeds/01-tables.js
@@ -0,0 +1,7 @@
+
+exports.seed = function (knex) {
+  return knex.raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+  .then(function () {
+    return knex("tables").insert(tables);
+  });
+  };
