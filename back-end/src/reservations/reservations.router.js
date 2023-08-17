c6b1820993b513efc847a80c3cc3e353965c014d
sleon204 <sleon87@gmail.com>
2023-08-17 08:05:21 -0700

    Update reservations.router.js
    added method not allowed

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,7 +6,8 @@
 
 const router = require('express').Router();
 const controller = require('./reservations.controller');
+const methodNotAllowed = require('../errors/methodNotAllowed');
 
-router.route('/').post(controller.create).get(controller.list);
+router.route('/').post(controller.create).get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
