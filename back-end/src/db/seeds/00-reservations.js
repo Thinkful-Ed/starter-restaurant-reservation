776b81e9eefc65a2378a68e047d43817442d8aa2
sleon204 <sleon87@gmail.com>
2023-08-16 21:28:15 -0700

    Update 00-reservations.js
    switched reservations to data to avoid confusion or overuse of reservation/s

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,8 +1,8 @@
-const reservations = require("./00-reservations.json");
+const data = require("./00-reservations.json");
 
 exports.seed = function (knex) {
   return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
   .then(function () {
-    return knex("reservations").insert(reservations);
+    return knex("reservations").insert(data);
   });
 };
