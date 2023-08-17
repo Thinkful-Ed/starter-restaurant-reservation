187d55c22acaf92c3f7d6bfd2824ab748912e98e
sleon204 <sleon87@gmail.com>
2023-08-16 21:31:23 -0700

    Update Dashboard.js
    implemented handlers and reservationslist

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
-import ErrorAlert from "../layout/ErrorAlert";
+import React, { useEffect, useState } from 'react';
+import { listReservations } from '../utils/api';
+import ErrorAlert from '../layout/ErrorAlert';
+import useQuery from '../utils/useQuery';
+import { previous, next, today } from '../utils/date-time';
+import { useHistory } from 'react-router-dom';
+import ReservationsList from './ReservationsList';
 
 /**
  * Defines the dashboard page.
@@ -9,30 +13,64 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
+	const history = useHistory();
+	const query = useQuery();
+	const queryDate = query.get('date');
+	const [reservations, setReservations] = useState([]);
+	const [reservationsError, setReservationsError] = useState(null);
 
-  useEffect(loadDashboard, [date]);
+	useEffect(loadDashboard, [queryDate]);
 
-  function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
-    listReservations({ date }, abortController.signal)
-      .then(setReservations)
-      .catch(setReservationsError);
-    return () => abortController.abort();
-  }
+	function loadDashboard() {
+		const abortController = new AbortController();
+		setReservationsError(null);
+		listReservations({ date: queryDate || today() }, abortController.signal)
+			.then(setReservations)
+			.catch(setReservationsError);
+		return () => abortController.abort();
+	}
 
-  return (
-    <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
-      </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
-    </main>
-  );
+	const handlePrevious = (event) => {
+		event.preventDefault();
+		history.push(`/dashboard?date=${previous(date)}`);
+	};
+
+	const handleNext = (event) => {
+		event.preventDefault();
+		history.push(`/dashboard?date=${next(date)}`);
+	};
+
+	const handleToday = (event) => {
+		event.preventDefault();
+
+		history.push(`/dashboard?date=${today()}`);
+	};
+
+	return (
+		<main>
+			<div className="text-center">
+				<h1>Dashboard</h1>
+				<div className="card d-md-flex mb-3">
+          <div className=" card-header mb-0">
+					<h4>
+						Reservations for {queryDate || today()}
+					</h4>
+						<button className="" onClick={handlePrevious}>
+							Previous
+						</button>
+						<button className="" onClick={handleToday}>
+							Today
+						</button>
+						<button className="" onClick={handleNext}>
+							Next
+						</button>
+          </div>
+					<ReservationsList reservations={reservations} date={queryDate || today()} />
+				</div>
+				<ErrorAlert error={reservationsError} />
+			</div>
+		</main>
+	);
 }
 
 export default Dashboard;
