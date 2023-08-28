56e1265248e7c76f9b84ee463c55be1109f8db03
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-08-28 15:47:39 -0400

    seeded tables

--- back-end/src/db/migrations/20230813153933_createTables.js
+++ back-end/src/db/migrations/20230813153933_createTables.js
@@ -7,6 +7,13 @@ exports.up = function(knex) {
     table.increments("table_id").primary();
     table.string("table_name").notNullable();
     table.integer("capacity").notNullable();
+    table.string("status").notNullable();
+    table.integer("reservation_id").unsigned();
+    table.foreign("reservation_id")
+      .references("reservation_id")
+      .inTable("reservations")
+      .onDelete("SET NULL");
+    table.timestamps(true, true);
   })
 };
 
