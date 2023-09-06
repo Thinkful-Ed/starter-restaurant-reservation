d2a20e742474cc1a95372c96f564665ce56f0bb0
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-09-05 20:07:40 -0400

    changed sidebar pos to fixed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
+import './App.css'
 
 /**
  * Defines the root application component.
