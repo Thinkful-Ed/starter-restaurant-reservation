ad5007f0b023f1a5e7e44f41ea36b92acf106a61
jackymiao <55778701+jackymiao@users.noreply.github.com>
2023-09-26 15:48:14 -0500

    finished US-01 backend

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,49 @@
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+const propertiesNotEmpty = require("../errors/propertiesNotEmpty");
+const validDateAndNumber = require("../errors/validDateAndNumber");
 /**
  * List handler for reservation resources
  */
+
+async function reservationExist(req,res,next){
+  const {reservation_id} = req.params;
+  const data = await service.read(reservation_id);
+  if(data){
+    res.locals.reservation = data;
+    next()
+  }else{
+    return next({status:404, message:`Reservation ${reservation_id} cannot be found.`})
+  }
+}
+
 async function list(req, res) {
+  const {date} = req.query;
+  const data = await service.list(date);
   res.json({
-    data: [],
+    data: data,
   });
 }
 
+async function read(req,res,next){
+  const {reservation_id} = req.params;
+  const data = await service.read(reservation_id)
+  res.json({
+    data:data
+  })
+}
+
+async function create(req,res,next){
+  const newReservation = req.body.data;
+  res.status(201).json({
+    data:newReservation
+  })
+}
+
 module.exports = {
-  list,
+  list:asyncErrorBoundary(list),
+  read:[asyncErrorBoundary(reservationExist),asyncErrorBoundary(read)],
+  create:[hasProperties('first_name','last_name','mobile_number','reservation_date','reservation_time','people'), propertiesNotEmpty('first_name','last_name','mobile_number','reservation_date','reservation_time'), validDateAndNumber, asyncErrorBoundary(create)],
 };
+
--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -6,7 +6,12 @@
 
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
+router.route("/")
+      .get(controller.list)
+      .post(controller.create)
+      .all(methodNotAllowed);
 
-router.route("/").get(controller.list);
-
+router.route("/:reservation_id")
+      .get(controller.read)
 module.exports = router;
