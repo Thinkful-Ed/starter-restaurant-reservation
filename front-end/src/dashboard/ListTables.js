2b77f5f797b47c2ee21a5cd41ec575c8e3d6dae5
Stephani <stephdb15@gmail.com>
2023-11-06 20:55:28 -0500

    fixing all warnings, ready for deploy

--- front-end/src/dashboard/ListTables.js
+++ front-end/src/dashboard/ListTables.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { finishTable } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 
@@ -21,64 +21,50 @@ export default function ListTables({ tables, loadDashboard, loadTables }) {
     }
   }
 
-  const displayTables = tables.sort().map((table, index) => {
-    return (
-      <>
-        {table.occupied || table.reservation_id ? (
-          <tr key={table.table_id}>
-            <th scope="row">{table.table_id}</th>
-            <td>{table.table_name}</td>
-            <td>{table.capacity}</td>
-            <td>{table.reservation_id}</td>
-            <td>
-              <p data-table-id-status={table.table_id}>Occupied</p>
-            </td>
-            <td>
-              <button
-                data-table-id-finish={table.table_id}
-                className="btn btn-outline-primary"
-                onClick={() => handleFinished(table.table_id)}
-              >
-                Finish
-              </button>
-            </td>
-          </tr>
-        ) : (
-          <tr key={index}>
-            <th scope="row">{table.table_id}</th>
-            <td>{table.table_name}</td>
-            <td>{table.capacity}</td>
-            <td>{table.reservation_id}</td>
-            <td>
-              <p className="col" data-table-id-status={table.table_id}>
-                Free
-              </p>
-            </td>
-          </tr>
-        )}
-      </>
-    );
-  });
+  const displayTables = tables.sort().map((table, index) =>  {    
+    return( 
+    <Fragment key={table.table_id}>
+        {table.occupied || table.reservation_id?
+            <tr key={table.table_id}>
+                <th scope="row">{table.table_id}</th>
+                <td>{table.table_name}</td>
+                <td>{table.capacity}</td>
+                <td>{table.reservation_id}</td>
+                <td><p data-table-id-status={table.table_id}>Occupied</p></td>
+                <td><button data-table-id-finish={table.table_id} className="btn btn-outline-primary" onClick={() => handleFinished(table.table_id)}>Finish</button></td>
+            </tr> : 
+              <tr key={index}>
+              <th scope="row">{table.table_id}</th>
+              <td>{table.table_name}</td>
+              <td>{table.capacity}</td>
+              <td>{table.reservation_id}</td>
+              <td><p className="col" data-table-id-status={table.table_id}>Free</p></td>
+            </tr>}
+            </Fragment>
+            )})
 
-  return (
+        
+return (
     <>
-      <ErrorAlert error={finishTableError} />
-      <div>
-        <table className="table align-middle">
-          <caption>Tables</caption>
-          <thead>
-            <tr>
-              <th scope="col">ID #</th>
-              <th scope="col">Table Name</th>
-              <th scope="col">Table Capacity</th>
-              <th scope="col">Reservation ID#</th>
-              <th scope="col">Occupied?</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>{displayTables}</tbody>
-        </table>
-      </div>
-    </>
-  );
-}
+     
+        <ErrorAlert error={finishTableError} />
+            <div>
+            <table className="table align-middle">
+            <caption>Tables</caption>
+            <thead>
+                <tr>
+                <th scope="col">ID #</th>
+                <th scope="col">Table Name</th>
+                <th scope="col">Table Capacity</th>
+                <th scope="col">Reservation ID#</th>
+                <th scope="col">Occupied?</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {displayTables}
+            </tbody>
+            </table>
+        </div>
+    </>);
+};
