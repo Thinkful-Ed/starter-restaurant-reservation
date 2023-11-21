77290b689a4ae7e4fd01d6f1ce604ce899ee728a
Clarisse <clarisse421@berkeley.edu>
2023-11-21 15:33:27 -0500

    US-04 - added a get method for reservations/:reservation_id

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,6 +8,7 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 
 router.route("/").get(controller.list).post(controller.create);
+router.route("/:reservation_id").get(controller.read);
 router.route("/new").post(controller.create);
 
 module.exports = router;
