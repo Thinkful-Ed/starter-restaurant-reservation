0d92e3ee90099fbc676e014cb02253aca38c898c
Clarisse <clarisse421@berkeley.edu>
2023-11-28 16:46:08 -0500

    US-01 - removed frontend validations and used ErrorAlert to show backend errors

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { createReservation } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function ReservationForm() {
   const history = useHistory();
@@ -14,52 +15,9 @@ function ReservationForm() {
   };
 
   const [formData, setFormData] = useState(initialFormData);
-  const [isTuesday, setIsTuesday] = useState(false);
-  const [isPastDate, setIsPastDate] = useState(false);
-  const [isInvalidTime, setIsInvalidTime] = useState(false);
-
-  const validateReservationDate = (date) => {
-    setIsTuesday(false);
-    setIsPastDate(false);
-    const dateAsDateObject = new Date(date);
-    if (dateAsDateObject.getDay() === 1) {
-      setIsTuesday(true);
-      return false;
-    }
-    const today = new Date();
-    if (dateAsDateObject < today) {
-      setIsPastDate(true);
-      return false;
-    }
-    return true;
-  };
-
-  const validateReservationTime = (time) => {
-    setIsInvalidTime(false);
-    const timeSplit = time.split(":");
-    const reservationHour = Number(timeSplit[0]);
-    const reservationMinutes = Number(timeSplit[1]);
-    if (
-      reservationHour < 10 ||
-      (reservationHour === 10 && reservationMinutes < 30)
-    ) {
-      setIsInvalidTime(true);
-      return false;
-    }
-    if (
-      reservationHour > 21 ||
-      (reservationHour === 21 && reservationMinutes >= 31)
-    ) {
-      setIsInvalidTime(true);
-      return false;
-    }
-    return true;
-  };
+  const [reservationsErrors, setReservationsErrors] = useState(null);
 
   const onChangeHandler = (event) => {
-    setIsInvalidTime(false);
-    setIsPastDate(false);
-    setIsTuesday(false);
     const property = event.target.name;
     const value =
       property === "people" ? Number(event.target.value) : event.target.value;
@@ -75,73 +33,22 @@ function ReservationForm() {
     history.goBack();
   };
 
-  const validateExists = (value) => {
-    if (typeof value === "number") {
-      return value;
-    }
-    return value && value.trim();
-  };
-
-  const validateForm = (formData) => {
-    const errors = {};
-
-    if (!validateExists(formData["first_name"])) {
-      errors.first_name = "Please enter your first name.";
-    }
-
-    if (!validateExists(formData["last_name"])) {
-      errors.last_name = "Please enter your last name.";
-    }
-    if (!validateExists(formData["mobile_number"])) {
-      errors.mobile_number = "Please enter your mobile number.";
-    }
-    if (!validateExists(formData["reservation_date"])) {
-      errors.reservation_date = "Please enter your reservation date.";
-    }
-    if (!validateExists(formData["reservation_time"])) {
-      errors.reservation_time = "Please enter your reservation time.";
-    }
-    if (!validateExists(formData["people"])) {
-      errors.people = "Please enter your party size.";
-    }
-
-    return errors;
-  };
-
   const submitHandler = (event) => {
     event.preventDefault();
 
-    let errors = {};
-    errors = validateForm(formData);
+    const abortController = new AbortController();
 
-    if (Object.keys(errors).length > 0) {
-      //Displays any errors
-      Object.keys(errors).forEach((key) => {
-        //Find the specific error element
-        alert(errors[key]);
-      });
-      return false;
-    }
-
-    if (!validateReservationTime(formData["reservation_time"])) {
-      return false;
-    }
-
-    if (!validateReservationDate(formData["reservation_date"])) {
-      return false;
-    }
-
-    createReservation(formData)
-      .then(() => {
-        setFormData(initialFormData);
-      })
+    createReservation(formData, abortController.signal)
       .then(() => {
         history.push(`/dashboard?date=${formData.reservation_date}`);
-      });
+      })
+      .catch(setReservationsErrors);
+    return () => abortController.abort();
   };
 
   return (
     <div>
+      <ErrorAlert error={reservationsErrors} />
       <h1>Reservation Form</h1>
       <form>
         <label htmlFor="first_name">
@@ -180,22 +87,6 @@ function ReservationForm() {
           onChange={onChangeHandler}
           value={formData.reservation_date}
         />
-        {isTuesday ? (
-          <div className="alert alert-danger">
-            We're sorry, the restaurant is closed on Tuesdays. Please choose a
-            reservation date on another day of the week.
-          </div>
-        ) : (
-          <></>
-        )}
-        {isPastDate ? (
-          <div className="alert alert-danger">
-            You cannot make a reservation for a past date. Please choose a
-            future date.
-          </div>
-        ) : (
-          <></>
-        )}
         <label htmlFor="reservation_time">Time of Reservation</label>
         <input
           type="time"
@@ -204,15 +95,6 @@ function ReservationForm() {
           onChange={onChangeHandler}
           value={formData.reservation_time}
         />
-        {isInvalidTime ? (
-          <div className="alert alert-danger">
-            We're sorry. Our resturant opens at 10:30AM and takes reservations
-            until 9:30PM. Please choose a reservation time within our available
-            hours.
-          </div>
-        ) : (
-          <></>
-        )}
         <label htmlFor="people">Number of People in the party</label>
         <input
           type="number"
