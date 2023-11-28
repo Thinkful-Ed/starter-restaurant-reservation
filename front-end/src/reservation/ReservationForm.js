7c78362a19861e80be6003fc8ed57ddd46ede90d
Julian <juliansmart1996@gmail.com>
2023-11-28 15:53:24 -0500

    final commit for us-03-frontend

--- front-end/src/reservation/ReservationForm.js
+++ front-end/src/reservation/ReservationForm.js
@@ -11,28 +11,6 @@ function ReservationForm({ initialFormState, submitAction }) {
   const history = useHistory();
 
 
-
-  // const isValidTimeValidator = (selectedTime) => {
-  //   if (selectedTime) {
-  //     const openingTime = "10:30";
-  //     const closingTime = "21:30";
-  
-  //     const timeStamp = new Date();
-  //     const currentTime = timeStamp.getHours() * 60 + timeStamp.getMinutes();
-  //     const selectedTimeMinutes = parseInt(selectedTime.split(":")[0]) * 60 + parseInt(selectedTime.split(":")[1]);
-  
-  //     if (formData.reservation_date === timeStamp.toISOString().split('T')[0] && currentTime > selectedTimeMinutes) {
-  //       setIsValidTime(false);
-  //     } else if (selectedTimeMinutes >= parseInt(openingTime.split(":")[0]) * 60 + parseInt(openingTime.split(":")[1]) &&
-  //       selectedTimeMinutes <= parseInt(closingTime.split(":")[0]) * 60 + parseInt(closingTime.split(":")[1])) {
-  //       setIsValidTime(true);
-  //     } else {
-  //       setIsValidTime(false);
-  //     }
-  //   }
-  // };
-
-
   useEffect(() => {
     const isValidTimeValidator = (selectedTime) => {
       if (selectedTime) {
