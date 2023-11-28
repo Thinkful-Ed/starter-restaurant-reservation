fbcf57156040198dd65f8e7de5658d1822c14a70
Clarisse <clarisse421@berkeley.edu>
2023-11-28 16:39:22 -0500

    US-03 - added a check for if the reservation date is today and the time is in the future

--- back-end/src/errors/hasValidProperties.js
+++ back-end/src/errors/hasValidProperties.js
@@ -1,75 +1,107 @@
+const validateReservationDate = (reservationDate, today, sameDay) => {
+  //checks if reservation date is a date
+  if (reservationDate.match(/^\d{4}-\d{2}-\d{2}$/) === null) {
+    const error = new Error(`A valid reservation_date is required.`);
+    error.status = 400;
+    throw error;
+  }
+
+  //checks if reservation date is not a tuesday
+
+  const resDateAsDateObject = new Date(reservationDate);
+  if (resDateAsDateObject.getDay() === 1) {
+    const error = new Error(
+      `The reservation_date can't be a tuesday since the restaurant is closed on Tuesdays.`
+    );
+    error.status = 400;
+    throw error;
+  }
+
+  //if the date is today, makes sure the reservation time is at least 1 hour later than current time
+
+  if (reservationDate === today.toISOString().split("T")[0]) {
+    //see if the reservationTime is past reservationDate
+    return true;
+  } else {
+    if (resDateAsDateObject < today) {
+      const error = new Error(`The reservation_date must be a future date.`);
+      error.status = 400;
+      throw error;
+    }
+  }
+};
+
+const validateReservationTime = (time, today, sameDay) => {
+  //checks time is in the right format
+  if (time.match(/^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/) === null) {
+    const error = new Error(`A valid reservation_time is required.`);
+    error.status = 400;
+    throw error;
+  }
+  //extracts hour and minute from time object
+  const timeSplit = time.split(":");
+  const reservationHour = Number(timeSplit[0]);
+  const reservationMinutes = Number(timeSplit[1]);
+
+  //checks if the reservation is same day, if so checks if time is valid
+  if (sameDay) {
+    const todayHour = today.getHours();
+    const todayMinutes = today.getMinutes();
+    //if its past the time do this
+    if (
+      todayHour > reservationHour ||
+      (todayHour === reservationHour && todayMinutes + 1 > reservationMinutes)
+    ) {
+      const error = new Error(
+        `The reservation_time must be made at a future time today.`
+      );
+      error.status = 400;
+      throw error;
+    }
+    //if it not pass the time, keep going
+  }
+
+  //checks if reservation is after 10:30AM opening
+  if (
+    reservationHour < 10 ||
+    (reservationHour === 10 && reservationMinutes < 30)
+  ) {
+    const error = new Error(
+      `The reservation_time must be after opening at 10:30AM.`
+    );
+    error.status = 400;
+    throw error;
+  }
+
+  //checks if reservation is made at least 1 hour before 9:30PM closing
+  if (
+    reservationHour > 21 ||
+    (reservationHour === 21 && reservationMinutes >= 31)
+  ) {
+    const error = new Error(
+      `The reservation_time must be at least 1 hour before closing at 9:30PM.`
+    );
+    error.status = 400;
+    throw error;
+  }
+};
+
 function hasValidProperties(...properties) {
   return function (res, req, next) {
     const { data = {} } = res.body;
 
     try {
-      //RESERVATION DATE
-      const reservationDate = data["reservation_date"];
-
-      //checks if reservation date is a date
-      if (reservationDate.match(/^\d{4}-\d{2}-\d{2}$/) === null) {
-        const error = new Error(`A valid reservation_date is required.`);
-        error.status = 400;
-        throw error;
-      }
-
-      //checks if reservation date is not a tuesday
-      const resDateAsDateObject = new Date(reservationDate);
-      if (resDateAsDateObject.getDay() === 1) {
-        const error = new Error(
-          `The reservation_date can't be a tuesday since the restaurant is closed on Tuesdays.`
-        );
-        error.status = 400;
-        throw error;
-      }
-
-      //checks if a reservation date is a future date
       const today = new Date();
-      if (resDateAsDateObject < today) {
-        const error = new Error(`The reservation_date must be a future date.`);
-        error.status = 400;
-        throw error;
-      }
 
-      console.log("today", today);
-      console.log("resDateAsObject", resDateAsDateObject);
+      //RESERVATION DATE
 
+      //checks if reservation dates are valid and also returns if the date is the same day as today
+      const isSameDay = validateReservationDate(
+        data["reservation_date"],
+        today
+      );
       //RESERVATION TIME
-      //checks time is in the right format
-      const reservationTime = data["reservation_time"];
-      if (
-        reservationTime.match(/^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$/) === null
-      ) {
-        const error = new Error(`A valid reservation_time is required.`);
-        error.status = 400;
-        throw error;
-      }
-
-      //extracts hour and minute from time object
-      const time = data["reservation_time"];
-      const timeSplit = time.split(":");
-      const reservationHour = Number(timeSplit[0]);
-      const reservationMinutes = Number(timeSplit[1]);
-      if (
-        reservationHour < 10 ||
-        (reservationHour === 10 && reservationMinutes < 30)
-      ) {
-        const error = new Error(
-          `The reservation_time must be after opening at 10:30AM.`
-        );
-        error.status = 400;
-        throw error;
-      }
-      if (
-        reservationHour > 21 ||
-        (reservationHour === 21 && reservationMinutes >= 31)
-      ) {
-        const error = new Error(
-          `The reservation_time must be at least 1 hour before closing at 9:30PM.`
-        );
-        error.status = 400;
-        throw error;
-      }
+      validateReservationTime(data["reservation_time"], today, isSameDay);
 
       //PARTY SIZE
       //checks that people is a number
