c95ce55f8ffd68825602a85a2d1ee1879bd0671d
Clarisse <clarisse421@berkeley.edu>
2023-11-28 10:49:14 -0500

    US-04 - frontend tests for seat reservation page is working; all tests now passing

--- front-end/src/reservations/SeatReservation.js
+++ front-end/src/reservations/SeatReservation.js
@@ -3,34 +3,20 @@ import {
   useHistory,
   useParams,
 } from "react-router-dom/cjs/react-router-dom.min";
-import {
-  assignReservationToTable,
-  listTables,
-  readReservation,
-  readTable,
-} from "../utils/api";
+import { assignReservationToTable, listTables } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 //Page: /reservations/:reservation_id/seat
 function SeatReservation() {
   const history = useHistory();
   const { reservation_id } = useParams();
-  const [reservation, setReservation] = useState({});
-  const [, setReservationsError] = useState(null);
+  const [reservationsError, setReservationsError] = useState(null);
   const [tables, setTables] = useState([]);
   const [, setTablesError] = useState(null);
   const [selectedTable_id, setSelectedTable_id] = useState("");
-  const [selectedTableObj, setSelectedTableObj] = useState({});
 
   function loadPage() {
     const abortController = new AbortController();
-    setReservationsError(null);
-    console.log("reservation_id", reservation_id);
-    readReservation(reservation_id)
-      .then(setReservation)
-      .then(() => {
-        console.log("reservation", reservation);
-      })
-      .catch(setReservationsError);
     setTablesError(null);
     listTables(abortController.signal).then(setTables).catch(setTablesError);
     return () => abortController.abort();
@@ -45,54 +31,38 @@ function SeatReservation() {
       </option>
     );
   });
-  const validateReservationRequest = () => {
-    const errors = {};
 
-    //selected a table
-    if (selectedTable_id === "") {
-      errors.table_selection = "Please select a table";
-    }
-    //reservationId exists
-
-    //table capacity <= party-size
-    if (selectedTableObj.capacity < reservation.people) {
-      console.log("not enough table space");
-    }
-    // if (selectedTableObj.capacity)
-    //table not occupied
-  };
-
-  async function submitHandler(event) {
+  const submitHandler = (event) => {
     event.preventDefault();
+    const abortController = new AbortController();
 
-    const table = await readTable(selectedTable_id);
-    setSelectedTableObj(table);
-    console.log("table obj", selectedTableObj);
-    validateReservationRequest();
-    assignReservationToTable(selectedTable_id, reservation_id);
-  }
+    assignReservationToTable(
+      selectedTable_id,
+      reservation_id,
+      abortController.signal
+    )
+      .then(() => history.push(`/dashboard`))
+      .catch(setReservationsError);
+    return () => abortController.abort();
+  };
 
   const handleChange = (event) => {
     setSelectedTable_id(event.target.value);
-    console.log(event.target.value);
   };
 
   return (
     <div>
+      <ErrorAlert error={reservationsError} />
       <h1>Seat Reservation #{reservation_id}</h1>
       <form>
         <p>Choose a table for the reservation:</p>
-        <label htmlFor="table_selection">
-          <select
-            id="table_selection"
-            name="table_selection"
-            onChange={handleChange}
-          >
-            <option>Select a table</option>
+        <label htmlFor="table_id">
+          <select id="table_id" name="table_id" onChange={handleChange}>
+            <option value="">Select a table</option>
             {tableRows}
           </select>
         </label>
-        <button type="Submit" onClick={submitHandler}>
+        <button type="submit" onClick={submitHandler}>
           Submit
         </button>
         <button onClick={() => history.goBack()}>Cancel</button>
