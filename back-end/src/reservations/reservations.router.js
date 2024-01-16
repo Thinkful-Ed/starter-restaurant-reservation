58a726560c7de621e29e06c57aa32c0eb890ee8f
Vanessa <zdperfectx@gmail.com>
2024-01-16 15:24:41 -0600

    added routing in backend router

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,12 +1,16 @@
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
 /**
  * Defines the router for reservation resources.
  *
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./reservations.controller");
-
-router.route("/").get(controller.list);
+router.route("/")
+.get(controller.listByDate)
+.post(controller.create)
+.all(methodNotAllowed);
 
 module.exports = router;
