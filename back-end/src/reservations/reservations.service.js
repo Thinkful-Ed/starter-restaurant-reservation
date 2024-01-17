37ead2d1e2299c3bee1e985eba0db6276f427295
Vanessa <zdperfectx@gmail.com>
2024-01-16 18:23:59 -0600

    added select to service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -2,6 +2,7 @@ const knex = require("../db/connection");
 
 function listByDate(date) {
   return knex("reservations")
+    .select("*")
     .where({ reservation_date: date })
     .orderBy("reservation_time");
 }
