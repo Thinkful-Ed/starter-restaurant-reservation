9489199c678943b01b90fdb63ba0f0ad8ca5c451
Vanessa <zdperfectx@gmail.com>
2024-01-18 10:54:06 -0600

    added API to seat table

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -118,3 +118,15 @@ export async function createTable(table, signal) {
   };
   return await fetchJson(url, options, table);
 }
+
+export async function seatTable(reservation_id, table_id, signal) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: "PUT",
+    mode: "cors",
+    headers,
+    body: JSON.stringify({ data: { reservation_id } }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
