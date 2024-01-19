c6c3b663d4867eb63e9d9050aebf5a56bf037737
Tim <Azacar02@gmail.com>
2024-01-19 14:02:24 -0500

    Added migrations and seed for /tables

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,6 +1,7 @@
 const reservations = require("./00-reservations.json");
 
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
-  .then(() => knex("reservations").insert(reservations));
+  return knex
+    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(() => knex("reservations").insert(reservations));
 };
