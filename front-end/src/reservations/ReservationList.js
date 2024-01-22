02b609c951fae4fe178a0dfd8a215edb42d504d8
Vanessa <zdperfectx@gmail.com>
2024-01-22 15:49:27 -0600

    added edit button to reservation list

--- front-end/src/reservations/ReservationList.js
+++ front-end/src/reservations/ReservationList.js
@@ -1,52 +1,71 @@
 import React from "react";
-import { getBadgeVariantReservation, formatPhoneNumber } from "../utils/helpers";
+import {
+  getBadgeVariantReservation,
+  formatPhoneNumber,
+} from "../utils/helpers";
 
 function ReservationList({ reservations }) {
   let reservationsMap;
   if (reservations.length === 0) {
     reservationsMap = (
       <tr>
-        <td colSpan="7">There are no reservations for this day.</td>
+        <td colSpan="8">There are no reservations for this day.</td>
       </tr>
     );
   } else {
     reservationsMap = reservations.map((reservation) => (
       <tr key={reservation.reservation_id}>
-        <td className="align-middle">
-          {reservation.first_name} {reservation.last_name}
-        </td>
-        <td className="align-middle">{formatPhoneNumber(reservation.mobile_number)}</td>
-        <td className="align-middle">{reservation.reservation_date}</td>
-        <td className="align-middle">{reservation.reservation_time}</td>
-        <td className="align-middle">{reservation.people}</td>
         <td className="align-middle text-capitalize">
           <span
-            className={`badge bg-${getBadgeVariantReservation(reservation.status)}`}
+            className={`badge bg-${getBadgeVariantReservation(
+              reservation.status
+            )}`}
             data-reservation-id-status={reservation.reservation_id}
           >
             {reservation.status}
           </span>
         </td>
         <td className="align-middle">
-          {reservation.status === "booked" && (
+          {reservation.first_name} {reservation.last_name}
+        </td>
+        <td className="align-middle">
+          {formatPhoneNumber(reservation.mobile_number)}
+        </td>
+        <td className="align-middle">{reservation.reservation_date}</td>
+        <td className="align-middle">{reservation.reservation_time}</td>
+        <td className="align-middle">{reservation.people}</td>
+        <td className="align-middle">
+          <div className="d-flex justify-content-center">
+            {reservation.status === "booked" && (
+              <a
+                href={`/reservations/${reservation.reservation_id}/seat`}
+                type="button"
+                className="btn btn-outline-info btn-sm m-2"
+              >
+                Seat
+              </a>
+            )}
             <a
-              href={`/reservations/${reservation.reservation_id}/seat`}
+              href={`/reservations/${reservation.reservation_id}/edit`}
               type="button"
-              className="btn btn-outline-info btn-sm align-middle"
+              className="btn btn-outline-secondary btn-sm m-2"
             >
-              Seat
+              Edit
             </a>
-          )}
+          </div>
         </td>
       </tr>
     ));
   }
 
   return (
-    <div className="table-responsive table-hover">
-      <table className="table text-center table-sm mb-1 mt-1 table-striped border bg-light">
-        <thead>
+    <div className="table-responsive">
+      <table className="table table-hover table-bordered table-sm text-center">
+        <thead className="thead-light">
           <tr>
+            <th scope="col" className="align-middle">
+              Status
+            </th>
             <th scope="col" className="align-middle">
               Full Name
             </th>
@@ -63,9 +82,8 @@ function ReservationList({ reservations }) {
               Party Size
             </th>
             <th scope="col" className="align-middle">
-              Reservation Status
+              Actions
             </th>
-            <th scope="col" className="align-middle"></th>
           </tr>
         </thead>
         <tbody className="table-group-divider">{reservationsMap}</tbody>
