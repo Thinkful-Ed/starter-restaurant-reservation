3020335e1bf610feb1fb9e20f842eb1a9c967a69
Vanessa <zdperfectx@gmail.com>
2024-01-26 17:10:22 -0600

    removed console log

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -106,7 +106,6 @@ function isNotTuesday(req, res, next) {
 function dateIsNotInPast(req, res, next) {
   const { reservation_date, reservation_time } = req.body.data;
   const day = new Date(`${reservation_date} ${reservation_time}`);
-  console.log('Constructed Date:', day);
   if (day < new Date()) {
     return next({
       status: 400,
