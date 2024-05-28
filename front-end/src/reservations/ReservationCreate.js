fc5cf847abb51c0bafd3f832e9d364a2087f2689
Hal Anderson <halbert.anderson@verizon.net>
2024-05-28 00:40:51 -0400

    switched to node version 16.20.0 to get the front end to connect to the backend, updated the ReservationCreate.js file to include the form elements (first_name, last_name, mobile_number, reservation_date, reservation_time, and people), and useState hook for the state of the reserevation being created. Also, updated the submit handler and implemented a changeHandler. In the utils/api.js file created a createReservations() method to send POST requests to the backend API route /reservations

--- front-end/src/reservations/ReservationCreate.js
+++ front-end/src/reservations/ReservationCreate.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { useHistory } from "react-router-dom";
-
+import { createReservation } from "../utils/api";
 function ReservationCreate() {
   const history = useHistory();
+
+  const [reservation, setReservation] = useState({
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  });
 
   function cancelHandler() {
     history.push("/");
@@ -10,30 +19,128 @@ function ReservationCreate() {
 
   function submitHandler(event) {
     event.preventDefault();
-    history.push("/");
+    createReservation(reservation).then(()=> {
+        history.push("/")
+    });
   }
  
+function changeHandler({ target: { name, value } }) {
+    setReservation((previousReservation) => ({
+         ...previousReservation,
+         [name]: value,
+    }));
+}
+
   return (
     <main>
-        <h1>Create Reservation</h1>
-        <p>Later, input fields will be added here</p>
-        <form onSubmit={submitHandler}>
-            <div>
-                <button 
-                    type="button" 
-                    className="btn btn-secondary mr-2"
-                    onClick={cancelHandler}
-                
-                >Cancel
-                </button>
-
-                <button
-                    type="submit"
-                    className="btn btn-primary"
-                >Submit
-                </button>
+        <h1 className="mb-3">Create Reservation</h1>
+        <form onSubmit={submitHandler} className="mb-4">
+            <div className="row mb-3">
+                <div className="col-6 form-group">
+                    <label className="form-label" htmlFor="first_name">
+                        First Name
+                    </label>
+                    <input
+                        className="form-control"
+                        id="first_name"
+                        name="first_name"
+                        type="text"
+                        value={reservation.first_name}
+                        onChange={changeHandler}
+                        required={true}
+                       />
+                </div>
+                <div className="col-6">
+                    <label className="form-label" htmlFor="last_name">
+                        Last Name
+                    </label>
+                    <input
+                        className="form-control"
+                        id="last_name"
+                        name="last_name"
+                        type="text"
+                        value={reservation.last_name}
+                        onChange={changeHandler}
+                        required={true}
+                    />
+                </div>
             </div>
-        </form>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="mobile_number">
+                    Mobile Number
+                </label>
+                <input
+                    className="form-control"
+                    id="mobile_number"
+                    name="mobile_number"
+                    type="text"
+                    value={reservation.mobile_number}
+                    onChange={changeHandler}
+                    required={true}
+                />
+            </div>
+            <div className="row mb-3">
+                <div className="col-6 form-group">
+                    <label className="form-label" htmlFor="reservation_date">
+                        Reservation Date
+                    </label>
+                    <input
+                        className="form-control"
+                        id="reservation_date"
+                        name="reservatin_date"
+                        type="date"
+                        value={reservation.reservation_date}
+                        onChange={changeHandler}
+                        required={true}
+                    />
+                </div>
+                <div className="col-6">
+                    <label className="form-label" htmlFor="reservation_time">
+                        Reservation Time
+                    </label>
+                    <input
+                        className="form-control"
+                        id="reservation_time"
+                        name="reservatin_time"
+                        type="time"
+                        value={reservation.reservation_time}
+                        onChange={changeHandler}
+                        required={true}
+                    />
+                </div>
+            </div>
+            <div className="mb-3">
+                    <label className="form-label" htmlFor="people">
+                        People
+                    </label>
+                    <input  
+                        className="form-control"
+                        id="people"
+                        name="people"
+                        type="text"
+                        value={reservation.people}
+                        onChange={changeHandler}
+                        required={true}
+                    />
+            </div>
+
+            <div className="mb-3"> 
+                    <button         
+                        type="button" 
+                        className="btn btn-secondary mr-2"
+                        onClick={cancelHandler}
+                    >Cancel
+                    </button>
+                  
+
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                    >Submit
+                    </button>
+                
+            </div>    
+        </form>        
     </main>
   );
 
