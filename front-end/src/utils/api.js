cae25cae9ee0763a78e3caab5e3081860e549366
Hal Anderson <halbert.anderson@verizon.net>
2024-06-02 02:57:00 -0400

    Updated the frontend src/utils/api file to Get /reservations, created validation middleware functions in the reservation.controller.js file, edited the reservations/reservations.controller.js and reservations.router.js files to implement Get /reservations route.

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -2,8 +2,8 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-// import formatReservationDate from "./format-reservation-date";
-// import formatReservationTime from "./format-reservation-date";
+import formatReservationDate from "./format-reservation-date";
+import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
@@ -58,20 +58,20 @@ async function fetchJson(url, options, onCancel) {
  *  a promise that resolves to a possibly empty array of reservation saved in the database.
  */
 
-// export async function listReservations(params, signal) {
-//   const url = new URL(`${API_BASE_URL}/reservations`);
-//   Object.entries(params).forEach(([key, value]) =>
-//     url.searchParams.append(key, value.toString())
-//   );
-//   return await fetchJson(url, { headers, signal }, [])
-//     .then(formatReservationDate)
-//     .then(formatReservationTime);
-// }
+export async function listReservations(params, signal) {
+  const url = new URL(`${API_BASE_URL}/reservations`);
+  Object.entries(params).forEach(([key, value]) =>
+    url.searchParams.append(key, value.toString())
+  );
+  return await fetchJson(url, { headers, signal }, [])
+    .then(formatReservationDate)
+    .then(formatReservationTime);
+}
 
-export async function listReservations(signal) {
-    // return reservations;
-      return [];
-  }
+// export async function listReservations(signal) {
+//     // return reservations;
+//       return [];
+//   }
 
 
 export async function createReservation(reservation, signal) {
