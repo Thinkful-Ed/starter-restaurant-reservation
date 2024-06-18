2d40dc7e4b4f30bdae5eb0c9014e687c9cf2ee8b
Hal Anderson <halbert.anderson@verizon.net>
2024-06-17 21:37:27 -0400

    updating changes made on another computer

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,3 +1,7 @@
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
