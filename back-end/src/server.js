9683ba142f62efb0b0a997f88291a2c5dc2eb0f9
Vanessa <zdperfectx@gmail.com>
2024-08-06 10:49:01 -0500

    added configuration and connected frontend and backend

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,17 @@
-const { PORT = 5001 } = process.env;
+const path = require("path");
+
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+// const { PORT = 5001 } = process.env; //ADD BACK
+
+// temp vv
+
+const { PORT = 5001, NODE_ENV = "development" } = process.env;
+
+console.log(`Running in ${NODE_ENV} mode`);
+console.log("Database URL:", process.env.DATABASE_URL_DEVELOPMENT); // Adjust based on current mode
+
+// temp ^^
 
 const app = require("./app");
 const knex = require("./db/connection");
