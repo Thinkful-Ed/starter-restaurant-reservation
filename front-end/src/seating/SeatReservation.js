f3600ccd5b79612a9d0d50131a6b7ad831bcbbe1
Vanessa <zdperfectx@gmail.com>
2024-08-26 17:23:10 -0500

    adjust seat reservation design to match and error handling

--- front-end/src/seating/SeatReservation.js
+++ front-end/src/seating/SeatReservation.js
@@ -25,6 +25,12 @@ function SeatReservation() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (!tableId || tableId === "0") {
+      setSeatError({
+        message: "You must select a table before seating a reservation.",
+      });
+      return;
+    }
     const abortController = new AbortController();
     try {
       await seatTables(reservation_id, tableId);
@@ -37,47 +43,56 @@ function SeatReservation() {
 
   const tableOptions = tables.map((table) => (
     <option key={table.table_id} value={table.table_id}>
-      {table.table_name} - {table.capacity}
+      Name: {table.table_name} - Capacity: {table.capacity}
     </option>
   ));
 
   return (
-    <div>
-      <h2 className="p-4 m-4 text-center">Select a Table</h2>
-      <form
-        className="row g-3 p-4 m-4 flex w-50 mx-auto border custom-border-color rounded bg-light"
-        onSubmit={handleSubmit}
-      >
-        <div className="w-50">
-          <select 
-          className="custom-select" 
-          required={true}
-          name="table_id"
-          aria-label="Default select example"
-          onChange={handleChange}
-          >
-            <option defaultValue={0}>Select a Table</option>
-            {tableOptions}
-          </select>
-        </div>
-        <div className="ml-auto pr-3">
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              history.goBack();
-            }}
-            className="btn btn-outline-danger d-flex justify-content-end"
-          >
-            Cancel
-          </button>
-        </div>
-        <div className="">
-          <button type="submit" className="btn btn-outline-secondary d-flex justify-content-end">
-            Submit
-          </button>
-        </div>
-      </form>
-      <ErrorAlert error={seatError} />
+    <div className="container-fluid d-flex flex-column w-75">
+      <h1 className="p-4 m-2 text-center fs-1 fw-bold">Select a Table</h1>
+      <div className="d-flex row justify-content-center">
+        <form
+          className="p-4 m-4 rounded-4 bg-transparent"
+          style={{ width: "30rem" }}
+          onSubmit={handleSubmit}
+        >
+          <div className="row">
+            <div className="col-md-12 p-2">
+              <select
+                className="form-select form-control"
+                required
+                name="table_id"
+                aria-label="Default select example"
+                onChange={handleChange}
+              >
+                <option defaultValue={0}>Select a Table</option>
+                {tableOptions}
+              </select>
+            </div>
+          </div>
+          {/* Buttons */}
+          <div className="row">
+            <div className="col-6 p-2">
+              <button
+                onClick={(event) => {
+                  event.preventDefault();
+                  history.goBack();
+                }}
+                className="btn btn-outline-danger"
+              >
+                Cancel
+              </button>
+            </div>
+
+            <div className="col-6 p-2 d-flex justify-content-end">
+              <button type="submit" className="btn btn-info px-4">
+                Seat
+              </button>
+            </div>
+          </div>
+        </form>
+        <ErrorAlert error={seatError} />
+      </div>
     </div>
   );
 }
